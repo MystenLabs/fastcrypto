dcbbf4a38b33760062ff5ab396235e3cb4bf4b6e
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2022-09-21 07:10:26 -0700

    Update tests and merge hkdf

--- src/tests/hash_tests.rs
+++ src/tests/hash_tests.rs
@@ -1,11 +1,15 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::hash::{Sha256, HashFunction};
+use crate::hash::{HashFunction, Sha256};
 
 #[test]
 fn test_sha256() {
-    let data = hex::decode("301d56460954541aab6dd7ddc0dd08f8cb3ebd884784a0e797905107533cae62").unwrap();
+    let data =
+        hex::decode("301d56460954541aab6dd7ddc0dd08f8cb3ebd884784a0e797905107533cae62").unwrap();
     let digest = Sha256::digest(&data);
-    assert_eq!(digest.as_ref(), hex::decode("2196d60feda3cd3787885c10a905e11fae911c32a0eb67fd290ade5df7eab140").unwrap());
+    assert_eq!(
+        digest.as_ref(),
+        hex::decode("2196d60feda3cd3787885c10a905e11fae911c32a0eb67fd290ade5df7eab140").unwrap()
+    );
 }
