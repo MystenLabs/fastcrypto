cfb9b4d618dfb94d088b7fee5dcf8bd4d61cc1c7
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2022-10-03 10:54:05 +0200

    Only allow unsecure schemes in debug

--- src/lib.rs
+++ src/lib.rs
@@ -74,7 +74,7 @@ pub mod serde_helpers;
 /// away.
 ///
 /// Warning: All schemes in this file are completely unsafe to use in production.
-#[cfg(feature = "unsecure_schemes")]
+#[cfg(all(feature = "unsecure_schemes", debug_assertions))]
 pub mod unsecure;
 
 ////////////////////////////////////////////////////////////////
