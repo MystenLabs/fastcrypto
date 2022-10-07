8860490bbff74b27432c3432c1112cecf00cdf85
huitseeker <huitseeker@users.noreply.github.com>
2022-10-07 19:51:55 +0000

    deploy: 415a84b8377359da995b639ae3437fd052ec3a20

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["fastcrypto"];
+window.ALL_CRATES = ["fastcrypto","fastcrypto_derive"];
--- source-files.js
+++ source-files.js
@@ -1,4 +1,5 @@
 var sourcesIndex = JSON.parse('{\
-"fastcrypto":["",[],["aes.rs","bls12381.rs","bulletproofs.rs","ed25519.rs","error.rs","hash.rs","hmac.rs","lib.rs","private_seed.rs","pubkey_bytes.rs","secp256k1.rs","serde_helpers.rs","traits.rs"]]\
+"fastcrypto":["",[],["aes.rs","bls12381.rs","bulletproofs.rs","ed25519.rs","error.rs","hash.rs","hmac.rs","lib.rs","private_seed.rs","pubkey_bytes.rs","secp256k1.rs","serde_helpers.rs","traits.rs"]],\
+"fastcrypto_derive":["",[],["lib.rs"]]\
 }');
 createSourceSidebar();
