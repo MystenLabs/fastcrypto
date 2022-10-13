826add4587ec180d1ecc67a51d6bbc855ee3c2d6
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2022-10-13 19:45:56 +0200

    Prepare for update of fastcrypto version in sui (#109)
    * Make hash function output more generic
    
    * Remove digest with consumer
    
    * Make Hashable::DigestType an associated type instead of generic
    
    * Add digesttype as associated type
    
    * Keep using blake2b
    
    * Keep using keccak256
    
    * fmt
    
    * Ensure eq, clone and hash traits for digests
    
    * Ensure traits for DigestType of Hashable
    
    * Add convenience types for digests and hashfunctions to avoid using typenum everywhere
    
    * fmt
    
    * Remove usage of external trait
    
    * Let digest impl copy
    
    * Make digesttype impl copy
    
    * Digest should not consume the input
    
    * Add generic digest trait!
    
    * Revert removing digesttype from hashable
    
    * Add digest of iterator function
    
    * Add const digest length to generic digests
    
    * A digest is now always 32 bytes
    
    * Set once_cell version to match sui
    
    * Update hash
    
    * Fix signature version
    
    * Remove unnecessary clones
    
    * Remove unnecessary into
    
    * Use const for digest length
    
    * Reintroduce variable digest lengths as a constant
    
    * Remove unnecessary return statement
    
    * Set tokio version to match sui
    
    * Add sha512

--- fastcrypto/src/hash.rs
+++ fastcrypto/src/hash.rs
@@ -2,108 +2,136 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use base64ct::{Base64, Encoding};
-use digest::OutputSizeUser;
-use generic_array::{ArrayLength, GenericArray};
 use serde::{Deserialize, Serialize};
+use serde_with::serde_as;
 use std::fmt;
 
-/// Represents a hash digest of `DigestLength` bytes.
-#[derive(Hash, PartialEq, Eq, Clone, Deserialize, Serialize, Ord, PartialOrd)]
-pub struct Digest<DigestLength: ArrayLength<u8> + 'static>(pub GenericArray<u8, DigestLength>);
+/// Represents a digest of `DIGEST_LEN`bytes.
+#[serde_with::serde_as]
+#[derive(Hash, PartialEq, Eq, Clone, Serialize, Deserialize, Ord, PartialOrd, Copy)]
+pub struct Digest<const DIGEST_LEN: usize> {
+    #[serde_as(as = "[_; DIGEST_LEN]")]
+    pub digest: [u8; DIGEST_LEN],
+}
 
-impl<DigestLength: ArrayLength<u8> + 'static> Digest<DigestLength> {
+impl<const DIGEST_LEN: usize> Digest<DIGEST_LEN> {
+    /// Create a new digest containing the given bytes
+    pub fn new(digest: [u8; DIGEST_LEN]) -> Self {
+        Digest { digest }
+    }
+
     /// Copy the digest into a new vector.
     pub fn to_vec(&self) -> Vec<u8> {
-        self.0.to_vec()
+        self.digest.to_vec()
     }
 
     /// The size of this digest in bytes.
     pub fn size(&self) -> usize {
-        DigestLength::USIZE
+        DIGEST_LEN
     }
 }
 
-impl<DigestLength: ArrayLength<u8> + 'static> fmt::Debug for Digest<DigestLength> {
+impl<const DIGEST_LEN: usize> fmt::Debug for Digest<DIGEST_LEN> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        write!(f, "{}", Base64::encode_string(&self.0))
+        write!(f, "{}", Base64::encode_string(&self.digest))
     }
 }
 
-impl<DigestLength: ArrayLength<u8> + 'static> fmt::Display for Digest<DigestLength> {
+impl<const DIGEST_LEN: usize> fmt::Display for Digest<DIGEST_LEN> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         write!(
             f,
             "{}",
-            Base64::encode_string(&self.0)
-                .get(0..DigestLength::USIZE)
+            Base64::encode_string(&self.digest)
+                .get(0..DIGEST_LEN)
                 .unwrap()
         )
     }
 }
 
-impl<DigestLength: ArrayLength<u8> + 'static> AsRef<[u8]> for Digest<DigestLength> {
+impl<const DIGEST_LEN: usize> AsRef<[u8]> for Digest<DIGEST_LEN> {
     fn as_ref(&self) -> &[u8] {
-        self.0.as_ref()
+        self.digest.as_ref()
     }
 }
 
-/// This trait is implemented by all messages that can be hashed.
-pub trait Hashable<DigestLength: ArrayLength<u8> + 'static> {
-    fn digest<H: HashFunction<DigestLength>>(self) -> Digest<DigestLength>;
-}
-
-impl<DigestLength: ArrayLength<u8> + 'static> Hashable<DigestLength> for &[u8] {
-    /// Hash this data using the given hash function.
-    fn digest<H: HashFunction<DigestLength>>(self) -> Digest<DigestLength> {
-        H::digest(self)
+impl<const DIGEST_LEN: usize> From<Digest<DIGEST_LEN>> for [u8; DIGEST_LEN] {
+    fn from(digest: Digest<DIGEST_LEN>) -> Self {
+        digest.digest
     }
 }
 
 /// Trait implemented by hash functions providing a output of fixed length
-pub trait HashFunction<DigestLength: ArrayLength<u8>>: OutputSizeUser + Sized + Default {
+pub trait HashFunction<const DIGEST_LENGTH: usize>: Default {
+    /// The length of this hash functions digests in bytes.
+    const OUTPUT_SIZE: usize = DIGEST_LENGTH;
+
+    /// Create a new hash function of the given type
+    fn new() -> Self {
+        Self::default()
+    }
+
     /// Process the given data, and update the internal of the hash function.
-    fn update(&mut self, data: &[u8]);
+    fn update<Data: AsRef<[u8]>>(&mut self, data: Data);
 
     /// Retrieve result and consume hash function.
-    fn finalize(self) -> Digest<DigestLength>;
+    fn finalize(self) -> Digest<DIGEST_LENGTH>;
 
-    fn digest(data: &[u8]) -> Digest<DigestLength> {
+    /// Compute the digest of the given data and consume the hash function.
+    fn digest<Data: AsRef<[u8]>>(data: Data) -> Digest<DIGEST_LENGTH> {
         let mut h = Self::default();
         h.update(data);
         h.finalize()
     }
+
+    /// Compute a single digest from all slices in the iterator in order and consume the hash function.
+    fn digest_iterator<K: AsRef<[u8]>, I: Iterator<Item = K>>(iter: I) -> Digest<DIGEST_LENGTH> {
+        let mut h = Self::default();
+        iter.into_iter().for_each(|chunk| h.update(chunk.as_ref()));
+        h.finalize()
+    }
 }
 
+/// This trait is implemented by all messages that can be hashed.
+pub trait Hash<const DIGEST_LEN: usize> {
+    /// The type of the digest when this is hashed.
+    type TypedDigest: Into<Digest<DIGEST_LEN>> + Eq + std::hash::Hash + Copy;
+
+    fn digest(&self) -> Self::TypedDigest;
+}
+
+/// This wraps a `digest::Digest` as a `fastcrypto::hash::HashFunction`.
 #[derive(Default)]
-pub struct HashFunctionWrapper<Variant: digest::Digest + 'static>(Variant);
+pub struct HashFunctionWrapper<Variant: digest::Digest + 'static, const DIGEST_LEN: usize>(Variant);
 
-impl<Variant: digest::Digest + 'static> OutputSizeUser for HashFunctionWrapper<Variant> {
-    type OutputSize = Variant::OutputSize;
-}
-
-impl<Variant: digest::Digest + 'static + Default> HashFunction<Variant::OutputSize>
-    for HashFunctionWrapper<Variant>
+impl<Variant: digest::Digest + 'static + Default, const DIGEST_LEN: usize> HashFunction<DIGEST_LEN>
+    for HashFunctionWrapper<Variant, DIGEST_LEN>
 {
-    fn update(&mut self, data: &[u8]) {
+    fn update<Data: AsRef<[u8]>>(&mut self, data: Data) {
         self.0.update(data);
     }
 
-    fn finalize(self) -> Digest<Variant::OutputSize> {
-        Digest(self.0.finalize())
+    fn finalize(self) -> Digest<DIGEST_LEN> {
+        Digest {
+            digest: self.0.finalize().as_slice().try_into().unwrap(),
+        }
     }
 }
 
 /// SHA-2
-pub type Sha256 = HashFunctionWrapper<sha2::Sha256>;
+pub type Sha256 = HashFunctionWrapper<sha2::Sha256, 32>;
 
 /// SHA-3
-pub type Sha3_256 = HashFunctionWrapper<sha3::Sha3_256>;
+pub type Sha3_256 = HashFunctionWrapper<sha3::Sha3_256, 32>;
+
+/// SHA-512
+pub type Sha512 = HashFunctionWrapper<sha2::Sha512, 64>;
 
 /// KECCAK
-pub type Keccak256 = HashFunctionWrapper<sha3::Keccak256>;
+pub type Keccak256 = HashFunctionWrapper<sha3::Keccak256, 32>;
 
-/// BLAKE2
-pub type Blake2b<DigestLength> = HashFunctionWrapper<blake2::Blake2b<DigestLength>>;
+/// BLAKE2-256
+pub type Blake2b256 = HashFunctionWrapper<blake2::Blake2b<typenum::U32>, 32>;
 
 /// BLAKE3
 #[derive(Default)]
@@ -111,17 +139,14 @@ pub struct Blake3 {
     instance: blake3::Hasher,
 }
 
-impl OutputSizeUser for Blake3 {
-    type OutputSize = typenum::U32;
-}
-
-impl HashFunction<typenum::U32> for Blake3 {
-    fn update(&mut self, data: &[u8]) {
-        self.instance.update(data);
+impl HashFunction<32> for Blake3 {
+    fn update<Data: AsRef<[u8]>>(&mut self, data: Data) {
+        self.instance.update(data.as_ref());
     }
 
-    fn finalize(self) -> Digest<typenum::U32> {
-        let hash: [u8; 32] = self.instance.finalize().into();
-        Digest(hash.into())
+    fn finalize(self) -> Digest<32> {
+        Digest {
+            digest: self.instance.finalize().into(),
+        }
     }
 }
