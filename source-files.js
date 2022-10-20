ce7eefc984118eac3adb7f359b20cf9e15ee2d82
jonas-lj <jonas-lj@users.noreply.github.com>
2022-10-20 20:11:28 +0000

    deploy: e3f0612d4120ec3f5a0a02e7a84b98b6012864a1

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = JSON.parse('{\
-"fastcrypto":["",[],["aes.rs","bls12381.rs","bulletproofs.rs","ed25519.rs","error.rs","hash.rs","hmac.rs","lib.rs","private_seed.rs","pubkey_bytes.rs","secp256k1.rs","serde_helpers.rs","traits.rs"]],\
+"fastcrypto":["",[],["aes.rs","bls12377.rs","bls12381.rs","bulletproofs.rs","ed25519.rs","error.rs","hash.rs","hmac.rs","lib.rs","private_seed.rs","pubkey_bytes.rs","secp256k1.rs","serde_helpers.rs","traits.rs"]],\
 "fastcrypto_derive":["",[],["lib.rs"]],\
 "fastcrypto_proving":["",[],["conversions.rs","dummy_circuit.rs","lib.rs","verifier.rs"]]\
 }');
