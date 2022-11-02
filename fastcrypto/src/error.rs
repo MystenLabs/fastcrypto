f78b564503f9afe27f79a0dff1be0588794b7d4b
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2022-11-02 09:28:25 +0100

    Add documentation to error module

--- fastcrypto/src/error.rs
+++ fastcrypto/src/error.rs
@@ -1,28 +1,36 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+//! Collection of errors to be used in fastcrypto.
+//! 
+//! A function should validate its arguments and return an indicative errors where needed.
+//! However, once the function is executing the cryptographic protocol/algorithm (directly/
+//! indirectly) then it should not return explicit errors as it might leak private information.
+//! In those cases the function should return the opaque, general error [FastCryptoError::GeneralError].
+//! When in doubt, prefer [FastCryptoError::GeneralError].
+
 use thiserror::Error;
 
-// A function should validate its arguments and return an indicative errors where needed.
-// However, once the function is executing the cryptographic protocol/algorithm (directly/
-// indirectly) then it should not return explicit errors as it might leak private information.
-// In those cases the function should return the opaque, general error FastCryptoError::GeneralError.
-// When in doubt, prefer FastCryptoError::GeneralError.
-
+/// Collection of errors to be used in fastcrypto.
 #[derive(Error, Debug, PartialEq, Eq)]
 pub enum FastCryptoError {
+    /// Invalid value was given to the function
     #[error("Invalid value was given to the function")]
     InvalidInput,
 
+    /// Input is to short.
     #[error("Expected input of length at least {0}")]
     InputTooShort(usize),
 
+    /// Input is to long.
     #[error("Expected input of length at most {0}")]
     InputTooLong(usize),
 
+    /// Input length is wrong.
     #[error("Expected input of length exactly {0}")]
     InputLengthWrong(usize),
 
+    /// General opaque cryptographic error.
     #[error("General cryptographic error")]
     GeneralError,
 }
