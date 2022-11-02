d162d6c31233455f1932562b7ade2eaa8aed49ab
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2022-11-02 09:49:31 +0100

    Add documentation to encodings

--- fastcrypto/src/encoding.rs
+++ fastcrypto/src/encoding.rs
@@ -1,6 +1,15 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+//! Encodings of binary data such as Base64 and Hex.
+//!
+//! # Example
+//! ```rust
+//! # use fastcrypto::encoding::*;
+//! assert_eq!(Hex::encode("Hello world!"), "0x48656c6c6f20776f726c6421");
+//! assert_eq!(Base64::encode("Hello world!"), "SGVsbG8gd29ybGQh");
+//! ```
+
 use base64ct::Encoding as _;
 use eyre::{eyre, Result};
 use schemars::JsonSchema;
@@ -21,11 +30,15 @@ where
     Error::custom(format!("byte deserialization failed, cause by: {:?}", e))
 }
 
+/// All encodings implements this trait.
 pub trait Encoding {
+    /// Decode this encoding into bytes.
     fn decode(s: &str) -> Result<Vec<u8>>;
+    /// Encode bytes into a string.
     fn encode<T: AsRef<[u8]>>(data: T) -> String;
 }
 
+/// Base64 encoding.
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, JsonSchema)]
 #[serde(try_from = "String")]
 pub struct Base64(String);
@@ -40,30 +53,35 @@ impl TryFrom<String> for Base64 {
 }
 
 impl Base64 {
+    /// Decodes this Base64 encoding to bytes.
     pub fn to_vec(&self) -> Result<Vec<u8>, eyre::Report> {
         Self::decode(&self.0)
     }
-
+    /// Encodes bytes as a Base64.
     pub fn from_bytes(bytes: &[u8]) -> Self {
         Self(Self::encode(bytes))
     }
-
+    /// Get a string representation of this Base64 encoding.
     pub fn encoded(&self) -> String {
         self.0.clone()
     }
 }
 
+/// Hex string encoding.
 #[derive(Serialize, Deserialize, Debug, JsonSchema, Clone)]
 pub struct Hex(String);
 
 impl Hex {
+    /// Create a hex encoding from a string.
     #[cfg(test)]
     pub fn from_string(s: &str) -> Self {
         Hex(s.to_string())
     }
+    /// Decodes this hex encoding to bytes.
     pub fn to_vec(&self) -> Result<Vec<u8>, eyre::Report> {
         Self::decode(&self.0)
     }
+    /// Encodes bytes as a hex string.
     pub fn from_bytes(bytes: &[u8]) -> Self {
         Self(Self::encode(bytes))
     }
@@ -133,10 +151,12 @@ where
     }
 }
 
+/// Encodes bytes as a hex string using lower case characters.
 pub fn encode_bytes_hex<B: AsRef<[u8]>>(bytes: B) -> String {
     hex::encode(bytes.as_ref())
 }
 
+/// Decodes a hex string to bytes. Both upper and lower case characters are allowed in the hex string.
 pub fn decode_bytes_hex<T: for<'a> TryFrom<&'a [u8]>>(s: &str) -> Result<T> {
     let s = s.strip_prefix("0x").unwrap_or(s);
     let value = hex::decode(s)?;
