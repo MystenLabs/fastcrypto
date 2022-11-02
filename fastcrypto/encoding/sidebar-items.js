daa0d468e57ebcbb364f98ac768d4a842f80a944
jonas-lj <jonas-lj@users.noreply.github.com>
2022-11-02 12:36:58 +0000

    deploy: a357841910aca2783298f4011113631ac7b07cfd

--- fastcrypto/encoding/sidebar-items.js
+++ fastcrypto/encoding/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["decode_bytes_hex",""],["encode_bytes_hex",""]],"struct":[["Base64",""],["Hex",""]],"trait":[["Encoding",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["decode_bytes_hex","Decodes a hex string to bytes. Both upper and lower case characters are allowed in the hex string."],["encode_bytes_hex","Encodes bytes as a hex string using lower case characters."]],"struct":[["Base64","Base64 encoding."],["Hex","Hex string encoding."]],"trait":[["Encoding","Trait representing a general binary-to-string encoding."]]};
