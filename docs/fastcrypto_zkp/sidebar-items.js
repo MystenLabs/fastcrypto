cca8b125dea58336cdc430afb882fb23e81f9a6a
joyqvq <joyqvq@users.noreply.github.com>
2022-11-16 20:44:25 +0000

    deploy: 1b9d3af1e42caa772bc0c9d491186dff00df8e08

--- docs/fastcrypto_zkp/sidebar-items.js
+++ docs/fastcrypto_zkp/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["conversions","Conversions between arkworks <-> blst"],["verifier","Groth16 SNARK verifier"]]};
+window.SIDEBAR_ITEMS = {"mod":[["api","API that takes in serialized inputs"],["conversions","Conversions between arkworks <-> blst"],["verifier","Groth16 SNARK verifier"]]};
