306465d4fe04f6c26359d885f3b0a548b661de40
François Garillot <4142+huitseeker@users.noreply.github.com>
2022-11-28 09:13:50 -0500

    Remove support for BLS12-377 signing & bump library version (#249)
    1. while we do use the BLS12-377 curve for proving (for good reason), we do not use it for signing, and are unlikely to do so in the near to medium term (due to strict performance requirements),
    2. @benr-ml is about to introduce a completely distinct instance & implementation of this curve as a dependency, in the context of threshold signing (see [repo](https://github.com/benr-ml/celo-threshold-bls-rs/tree/test-perf))
    3. The implementations of BLS12-377 *as a signing scheme* are not as mature as other instances, notably in the area of hashing to the curve. Indeed, BLS12-377 does not yet have a suite in the [hash-to-curve RFC](https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html#suites).
    
    <details>
    <summary>Deep-dive into item 3</summary>
    
    https://github.com/celo-org/celo-bls-snark-rs
    - [lightly-maintained](https://github.com/celo-org/celo-bls-snark-rs/issues/228),
    - minSig: [PK in G2](https://github.com/celo-org/celo-bls-snark-rs/blob/master/crates/bls-crypto/src/bls/public.rs#L16)
    - uses [try-and-increment as hash to G1](https://github.com/celo-org/celo-bls-snark-rs/blob/master/crates/bls-crypto/src/hash_to_curve/mod.rs),
    
    https://github.com/w3f/bls
    - [lightly-maintained](https://github.com/w3f/bls/pull/14),
    - optionally, [minSig or minPK](https://github.com/w3f/bls/blob/master/src/engine.rs),
    - [customizable](https://github.com/w3f/bls/blob/19099c038c0ee2b5fb323c55d250742a95f28bf6/src/engine.rs#L102-L105) hash-to-curve,
    - allows an override to Wahby-Boneh / SSWU, but in [a PR](https://github.com/w3f/bls/pull/57),
    - includes a PoP scheme, which has an [issue](https://github.com/w3f/bls/issues/32).
    
    https://github.com/ConsenSys/gnark-crypto
    - in Go,
    - maintained,
    - implements an SSWU map (see [RFC](https://datatracker.ietf.org/doc/draft-irtf-cfrg-hash-to-curve/) section 6.6.2)
    - the parameters are [undocumented](https://github.com/ConsenSys/gnark-crypto/blob/master/ecc/bls12-377/hash_to_g1.go)
    - but the parameters may come from [this script](https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-hash-to-curve-06#appendix-E.2)
    </details>
    
    It seems best to spend a little bit of time thinking about BLS12-377, a scheme we don't really use, before adopting & maintaining a version of it. Once we sort out the map_to_curve part, it may be suitable to improve & build on the
    implementation we're set to receive from (2.) above (or to an upstream impl) rather than continuing with this specific variant of the code.
    
    Fixes #242.

--- fastcrypto/src/tests/mskr_tests.rs
+++ fastcrypto/src/tests/mskr_tests.rs
@@ -1,7 +1,6 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::bls12377::{BLS12377AggregateSignature, BLS12377KeyPair};
 use crate::bls12381::min_pk::{BLS12381AggregateSignature, BLS12381KeyPair};
 use crate::traits::mskr::Randomize;
 use crate::traits::{AggregateAuthenticator, KeyPair};
@@ -82,84 +81,6 @@ fn verify_aggregate_subset_bls12381_min_pk() {
         .map(|pk| pk.randomize(pk, &pks))
         .collect::<Vec<_>>();
     let aggregate_sig = BLS12381AggregateSignature::aggregate(&sigs).unwrap();
-
-    assert!(aggregate_sig.verify(&randomized_pks, msg).is_ok())
-}
-
-#[test]
-fn verify_randomized_signature_bls12377() {
-    let kp = BLS12377KeyPair::generate(&mut thread_rng());
-
-    let pks = (0..4)
-        .map(|_| {
-            let kp = BLS12377KeyPair::generate(&mut thread_rng());
-            kp.public().clone()
-        })
-        .collect::<Vec<_>>();
-
-    let msg = b"Hello world";
-
-    let randomized_kp = kp.randomize(kp.public(), &pks);
-    let sig = kp.sign(msg);
-
-    assert!(kp.public().verify(msg, &sig).is_ok());
-
-    assert!(randomized_kp.public().verify(msg, &sig).is_err());
-
-    let randomized_sig = randomized_kp.sign(msg);
-    assert!(randomized_kp.public().verify(msg, &randomized_sig).is_ok());
-
-    assert!(randomized_kp
-        .public()
-        .verify(msg, &sig.randomize(kp.public(), &pks))
-        .is_ok());
-}
-
-#[test]
-fn verify_aggregate_all_bls12377() {
-    let kps = (0..4)
-        .map(|_| BLS12377KeyPair::generate(&mut thread_rng()))
-        .collect::<Vec<_>>();
-
-    let pks = kps.iter().map(|kp| kp.public().clone()).collect::<Vec<_>>();
-
-    let msg: &[u8] = b"Hello, world!";
-    let sigs = kps
-        .iter()
-        .map(|kp| kp.randomize(kp.public(), &pks).sign(msg))
-        .collect::<Vec<_>>();
-
-    let randomized_pks = pks
-        .iter()
-        .map(|pk| pk.randomize(pk, &pks))
-        .collect::<Vec<_>>();
-
-    let aggregate_sig = BLS12377AggregateSignature::aggregate(&sigs).unwrap();
-
-    assert!(aggregate_sig.verify(&randomized_pks, msg).is_ok())
-}
-
-#[test]
-fn verify_aggregate_subset_bls12377() {
-    let kps = (0..4)
-        .map(|_| BLS12377KeyPair::generate(&mut thread_rng()))
-        .collect::<Vec<_>>();
-
-    let pks = kps.iter().map(|kp| kp.public().clone()).collect::<Vec<_>>();
-
-    let msg: &[u8] = b"Hello, world!";
-    let sigs = kps
-        .iter()
-        .skip(1)
-        .map(|kp| kp.randomize(kp.public(), &pks).sign(msg))
-        .collect::<Vec<_>>();
-
-    let randomized_pks = pks
-        .iter()
-        .skip(1)
-        .map(|pk| pk.randomize(pk, &pks))
-        .collect::<Vec<_>>();
-    let aggregate_sig = BLS12377AggregateSignature::aggregate(&sigs).unwrap();
 
     assert!(aggregate_sig.verify(&randomized_pks, msg).is_ok())
 }
