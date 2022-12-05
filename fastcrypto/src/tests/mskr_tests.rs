52f9e9ed25e6e2e83dcacff4842542c9f40c9150
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2022-12-05 13:35:50 +0100

    Add tests for min_sig

--- fastcrypto/src/tests/mskr_tests.rs
+++ fastcrypto/src/tests/mskr_tests.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::bls12381::min_pk::{BLS12381AggregateSignature, BLS12381KeyPair};
+use crate::bls12381::{min_pk, min_sig};
 use crate::traits::mskr::Randomize;
 use crate::traits::{AggregateAuthenticator, KeyPair};
 use rand::thread_rng;
@@ -9,11 +9,11 @@ use signature::{Signer, Verifier};
 
 #[test]
 fn verify_randomized_signature_bls12381_min_pk() {
-    let kp = BLS12381KeyPair::generate(&mut thread_rng());
+    let kp = min_pk::BLS12381KeyPair::generate(&mut thread_rng());
 
     let pks = (0..4)
         .map(|_| {
-            let kp = BLS12381KeyPair::generate(&mut thread_rng());
+            let kp = min_pk::BLS12381KeyPair::generate(&mut thread_rng());
             kp.public().clone()
         })
         .collect::<Vec<_>>();
@@ -39,7 +39,7 @@ fn verify_randomized_signature_bls12381_min_pk() {
 #[test]
 fn verify_aggregate_all_bls12381_min_pk() {
     let kps = (0..4)
-        .map(|_| BLS12381KeyPair::generate(&mut thread_rng()))
+        .map(|_| min_pk::BLS12381KeyPair::generate(&mut thread_rng()))
         .collect::<Vec<_>>();
 
     let pks = kps.iter().map(|kp| kp.public().clone()).collect::<Vec<_>>();
@@ -55,7 +55,7 @@ fn verify_aggregate_all_bls12381_min_pk() {
         .map(|pk| pk.randomize(pk, &pks))
         .collect::<Vec<_>>();
 
-    let aggregate_sig = BLS12381AggregateSignature::aggregate(&sigs).unwrap();
+    let aggregate_sig = min_pk::BLS12381AggregateSignature::aggregate(&sigs).unwrap();
 
     assert!(aggregate_sig.verify(&randomized_pks, msg).is_ok())
 }
@@ -63,7 +63,7 @@ fn verify_aggregate_all_bls12381_min_pk() {
 #[test]
 fn verify_aggregate_subset_bls12381_min_pk() {
     let kps = (0..4)
-        .map(|_| BLS12381KeyPair::generate(&mut thread_rng()))
+        .map(|_| min_pk::BLS12381KeyPair::generate(&mut thread_rng()))
         .collect::<Vec<_>>();
 
     let pks = kps.iter().map(|kp| kp.public().clone()).collect::<Vec<_>>();
@@ -80,7 +80,85 @@ fn verify_aggregate_subset_bls12381_min_pk() {
         .skip(1)
         .map(|pk| pk.randomize(pk, &pks))
         .collect::<Vec<_>>();
-    let aggregate_sig = BLS12381AggregateSignature::aggregate(&sigs).unwrap();
+    let aggregate_sig = min_pk::BLS12381AggregateSignature::aggregate(&sigs).unwrap();
+
+    assert!(aggregate_sig.verify(&randomized_pks, msg).is_ok())
+}
+
+#[test]
+fn verify_randomized_signature_bls12381_min_sig() {
+    let kp = min_sig::BLS12381KeyPair::generate(&mut thread_rng());
+
+    let pks = (0..4)
+        .map(|_| {
+            let kp = min_sig::BLS12381KeyPair::generate(&mut thread_rng());
+            kp.public().clone()
+        })
+        .collect::<Vec<_>>();
+
+    let msg = b"Hello world";
+
+    let randomized_kp = kp.randomize(kp.public(), &pks);
+    let sig = kp.sign(msg);
+
+    assert!(kp.public().verify(msg, &sig).is_ok());
+
+    assert!(randomized_kp.public().verify(msg, &sig).is_err());
+
+    let randomized_sig = randomized_kp.sign(msg);
+    assert!(randomized_kp.public().verify(msg, &randomized_sig).is_ok());
+
+    assert!(randomized_kp
+        .public()
+        .verify(msg, &sig.randomize(kp.public(), &pks))
+        .is_ok());
+}
+
+#[test]
+fn verify_aggregate_all_bls12381_min_sig() {
+    let kps = (0..4)
+        .map(|_| min_sig::BLS12381KeyPair::generate(&mut thread_rng()))
+        .collect::<Vec<_>>();
+
+    let pks = kps.iter().map(|kp| kp.public().clone()).collect::<Vec<_>>();
+
+    let msg: &[u8] = b"Hello, world!";
+    let sigs = kps
+        .iter()
+        .map(|kp| kp.randomize(kp.public(), &pks).sign(msg))
+        .collect::<Vec<_>>();
+
+    let randomized_pks = pks
+        .iter()
+        .map(|pk| pk.randomize(pk, &pks))
+        .collect::<Vec<_>>();
+
+    let aggregate_sig = min_sig::BLS12381AggregateSignature::aggregate(&sigs).unwrap();
+
+    assert!(aggregate_sig.verify(&randomized_pks, msg).is_ok())
+}
+
+#[test]
+fn verify_aggregate_subset_bls12381_min_sig() {
+    let kps = (0..4)
+        .map(|_| min_sig::BLS12381KeyPair::generate(&mut thread_rng()))
+        .collect::<Vec<_>>();
+
+    let pks = kps.iter().map(|kp| kp.public().clone()).collect::<Vec<_>>();
+
+    let msg: &[u8] = b"Hello, world!";
+    let sigs = kps
+        .iter()
+        .skip(1)
+        .map(|kp| kp.randomize(kp.public(), &pks).sign(msg))
+        .collect::<Vec<_>>();
+
+    let randomized_pks = pks
+        .iter()
+        .skip(1)
+        .map(|pk| pk.randomize(pk, &pks))
+        .collect::<Vec<_>>();
+    let aggregate_sig = min_sig::BLS12381AggregateSignature::aggregate(&sigs).unwrap();
 
     assert!(aggregate_sig.verify(&randomized_pks, msg).is_ok())
 }
