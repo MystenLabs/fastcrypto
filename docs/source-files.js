c5736992627b444e516fd9257054bb810cac0010
jonas-lj <jonas-lj@users.noreply.github.com>
2022-12-06 06:08:51 +0000

    deploy: df4da3e40cf9b5b9e9fb936b41820216fd63d352

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = JSON.parse('{\
-"fastcrypto":["",[["groups",[],["mod.rs","ristretto255.rs"]]],["aes.rs","bls12381.rs","bulletproofs.rs","ed25519.rs","encoding.rs","error.rs","hash.rs","hmac.rs","lib.rs","polynomial.rs","private_seed.rs","pubkey_bytes.rs","secp256k1.rs","secp256r1.rs","serde_helpers.rs","traits.rs"]],\
+"fastcrypto":["",[["bls12381",[["min_pk",[],["mod.rs"]],["min_sig",[],["mod.rs"]]],["mod.rs"]],["groups",[],["mod.rs","ristretto255.rs"]]],["aes.rs","bulletproofs.rs","ed25519.rs","encoding.rs","error.rs","hash.rs","hmac.rs","lib.rs","polynomial.rs","private_seed.rs","pubkey_bytes.rs","secp256k1.rs","secp256r1.rs","serde_helpers.rs","traits.rs"]],\
 "fastcrypto_derive":["",[],["lib.rs"]],\
 "fastcrypto_zkp":["",[],["api.rs","conversions.rs","dummy_circuits.rs","lib.rs","verifier.rs"]]\
 }');
