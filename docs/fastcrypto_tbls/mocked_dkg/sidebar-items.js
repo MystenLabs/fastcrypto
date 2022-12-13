6bf02db2ca49527c9a8b94a1fb8b4d164b8e3852
benr-ml <benr-ml@users.noreply.github.com>
2022-12-13 07:40:05 +0000

    deploy: a31b60867051b38a19b0217edd123081b4269aa8

---
+++ docs/fastcrypto_tbls/mocked_dkg/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["generate_full_key_pair","Emulate the output of the previous key recovery protocol."],["generate_public_keys","Emulate the public output of DKG."],["generate_share_and_public_keys","Emulate the output of DKG for a given id."]],"type":[["PrivateBlsKey",""],["PublicBlsKey",""],["PublicVssKey",""],["Share",""]]};
