7dfa27aeb566169a76b832d38c78d7513c7aea16
benr-ml <benriva@mystenlabs.com>
2022-12-27 22:37:23 +0200

    add header

--- fastcrypto/src/base64_representation.rs
+++ fastcrypto/src/base64_representation.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::encoding::{Base64, Encoding};
 use crate::error::FastCryptoError;
 use crate::groups::bls12381::Base64G1Element;
