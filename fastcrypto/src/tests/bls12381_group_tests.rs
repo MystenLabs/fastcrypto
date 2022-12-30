957ebf446fdc000d5841c72725ba2f8ac39f7313
benr-ml <112846738+benr-ml@users.noreply.github.com>
2022-12-30 11:03:48 +0200

    Check consistent serialization of BLS12-381 (#323)

--- fastcrypto/src/tests/bls12381_group_tests.rs
+++ fastcrypto/src/tests/bls12381_group_tests.rs
@@ -1,10 +1,14 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::bls12381::min_pk::{BLS12381KeyPair, BLS12381Signature};
 use crate::groups::bls12381::{
     G1Element, G2Element, GTElement, Scalar, G_1_ELEMENT_BYTE_LENGTH, G_2_ELEMENT_BYTE_LENGTH,
 };
 use crate::groups::{GroupElement, HashToGroupElement, Pairing};
+use crate::traits::{KeyPair, ToFromBytes};
+use rand::{rngs::StdRng, SeedableRng as _};
+use signature::{Signer, Verifier};
 
 const MSG: &[u8] = b"test message";
 
@@ -139,4 +143,27 @@ fn test_g2_serialize_deserialize() {
     let serialized = bincode::serialize(&p).unwrap();
     let deserialized: G2Element = bincode::deserialize(&serialized).unwrap();
     assert_eq!(deserialized, p);
+}
+
+#[test]
+fn test_consistent_bls12381_serialization() {
+    // Generate with BLS signature APIs.
+    let pair = BLS12381KeyPair::generate(&mut StdRng::from_seed([0; 32]));
+    let (pk1, sk1) = (pair.public().clone(), pair.private());
+    let sig1 = sk1.sign(MSG); // encoded in G1.
+
+    // Convert using serialized byte arrays.
+    let pk2: G1Element = bincode::deserialize(pk1.as_ref()).unwrap();
+    let sig2: G2Element = bincode::deserialize(sig1.as_ref()).unwrap();
+    // Check signature with pk2, sig2.
+    assert_eq!(
+        pk2.pairing(&G2Element::hash_to_group_element(MSG)),
+        G1Element::generator().pairing(&sig2)
+    );
+
+    // Convert back and check the resulting signature.
+    let sig2_as_bytes = bincode::serialize(&sig2).unwrap();
+    let sig3 = <BLS12381Signature as ToFromBytes>::from_bytes(sig2_as_bytes.as_slice()).unwrap();
+    pk1.verify(MSG, &sig3).unwrap();
+    assert_eq!(sig1, sig3);
 }
