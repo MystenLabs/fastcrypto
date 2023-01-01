a34e027b4fee83651c1db716541ad5c2cad88f32
benr-ml <benr-ml@users.noreply.github.com>
2023-01-01 09:25:54 +0000

    deploy: ebafc160dd5fb9f787cb2cdd8276d3f821f7d06e

--- docs/fastcrypto/serde_helpers/sidebar-items.js
+++ docs/fastcrypto/serde_helpers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["keypair_decode_base64",""]],"mod":[["min_pk",""],["min_sig",""]],"struct":[["BytesRepresentation","External types."],["Ed25519Signature",""],["SerializationHelper","Serialization of internal types."]],"trait":[["ToFromByteArray",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["keypair_decode_base64",""]],"struct":[["BytesRepresentation","External types."],["Ed25519Signature",""],["SerializationHelper","Serialization of internal types."]],"trait":[["ToFromByteArray",""]]};
