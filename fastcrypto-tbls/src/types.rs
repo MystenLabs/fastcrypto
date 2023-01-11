8a78a1ec09c05e2e8f115557d1561684a3693dfb
benr-ml <benriva@mystenlabs.com>
2023-01-11 18:37:49 +0200

    add an alias

--- fastcrypto-tbls/src/types.rs
+++ fastcrypto-tbls/src/types.rs
@@ -1,10 +1,13 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::tbls;
+use crate::polynomial::{Eval, PublicPoly};
+use crate::{ecies, tbls};
 use fastcrypto::error::FastCryptoError;
+use fastcrypto::groups::ristretto255::RistrettoPoint;
 use fastcrypto::groups::{bls12381, GroupElement, HashToGroupElement, Pairing};
-use fastcrypto::polynomial::{Eval, PublicPoly};
+use serde::{Deserialize, Serialize};
+use std::num::NonZeroU32;
 
 /// Implementation of [ThresholdBls] for BLS12-381-min-sig. A variant for BLS12-381-min-pk can be
 /// defined in a similar way if needed in the future.
@@ -32,10 +35,28 @@ impl tbls::ThresholdBls for ThresholdBls12381MinSig {
     }
 }
 
-// Shorter to use types.
+/// tBLS with ThresholdBls12381MinSig types.
+///
 pub type Share = Eval<<ThresholdBls12381MinSig as tbls::ThresholdBls>::Private>;
 pub type PrivateBlsKey = <ThresholdBls12381MinSig as tbls::ThresholdBls>::Private;
 pub type PublicBlsKey = <ThresholdBls12381MinSig as tbls::ThresholdBls>::Public;
 pub type PublicVssKey = PublicPoly<<ThresholdBls12381MinSig as tbls::ThresholdBls>::Public>;
 pub type Signature = <ThresholdBls12381MinSig as tbls::ThresholdBls>::Signature;
 pub type RawSignature = bls12381::G1ElementAsBytes;
+
+/// Indexes of shares/private keys (0 is reserved for the secret itself).
+pub type ShareIndex = NonZeroU32;
+
+/// Wrapper of a value that is associated with a specific index.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct IndexedValue<A> {
+    pub index: ShareIndex,
+    pub value: A,
+}
+
+/// ECIES related types with Ristretto points.
+///
+pub type PrivateEciesKey = ecies::PrivateKey<RistrettoPoint>;
+pub type PublicEciesKey = ecies::PublicKey<RistrettoPoint>;
+pub type EciesEncryption = ecies::Encryption<RistrettoPoint>;
+pub type RecoveryPackage = ecies::RecoveryPackage<RistrettoPoint>;
