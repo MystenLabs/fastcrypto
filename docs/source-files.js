4cc80e54deb7f0ea58cf214b89d21633810fbd25
jonas-lj <jonas-lj@users.noreply.github.com>
2023-01-23 12:29:18 +0000

    deploy: 79a8c6c6c99e2ebf208df7c6ab2b79dec057d1c8

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = JSON.parse('{\
-"fastcrypto":["",[["bls12381",[["min_pk",[],["mod.rs"]],["min_sig",[],["mod.rs"]]],["mod.rs"]],["groups",[],["bls12381.rs","mod.rs","ristretto255.rs"]]],["aes.rs","bulletproofs.rs","ed25519.rs","encoding.rs","error.rs","hash.rs","hmac.rs","lib.rs","private_seed.rs","secp256k1.rs","secp256r1.rs","serde_helpers.rs","signature_service.rs","traits.rs"]],\
+"fastcrypto":["",[["bls12381",[["min_pk",[],["mod.rs"]],["min_sig",[],["mod.rs"]]],["mod.rs"]],["groups",[],["bls12381.rs","mod.rs","ristretto255.rs"]],["secp256k1",[],["mod.rs","recoverable.rs"]]],["aes.rs","bulletproofs.rs","ed25519.rs","encoding.rs","error.rs","hash.rs","hmac.rs","lib.rs","private_seed.rs","secp256r1.rs","serde_helpers.rs","signature_service.rs","traits.rs"]],\
 "fastcrypto_derive":["",[],["lib.rs"]],\
 "fastcrypto_tbls":["",[],["ecies.rs","lib.rs","mocked_dkg.rs","polynomial.rs","random_oracle.rs","tbls.rs","types.rs"]],\
 "fastcrypto_zkp":["",[],["api.rs","conversions.rs","dummy_circuits.rs","lib.rs","verifier.rs"]]\
