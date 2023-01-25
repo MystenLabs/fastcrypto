a66df8430838d3156df3fd55aa0fd020ffe7227d
jonas-lj <jonas-lj@users.noreply.github.com>
2023-01-25 17:37:29 +0000

    deploy: d53b0cd110f5d1404cb49104d07c8f8e2cc4b61c

--- docs/fastcrypto/secp256r1/sidebar-items.js
+++ docs/fastcrypto/secp256r1/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["recoverable","This module contains an implementation of the ECDSA signature scheme over the secp256r1 NIST-P1 curve. The nonce is generated deterministically according to RFC6979."]]};
+window.SIDEBAR_ITEMS = {"mod":[["recoverable","This module contains an implementation of the ECDSA signature scheme over the secp256r1 NIST-P1 curve where the public key can be recovered from a signature. The nonce is generated deterministically according to RFC6979."]]};
