8b84e79d732addcd74ea1ad89f417d5ae44a24f7
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-02-01 14:17:33 +0100

    Do not use optional multiscalar multiplication

--- fastcrypto/src/groups/ristretto255.rs
+++ fastcrypto/src/groups/ristretto255.rs
@@ -16,7 +16,7 @@ use curve25519_dalek_ng::constants::{BASEPOINT_ORDER, RISTRETTO_BASEPOINT_POINT}
 use curve25519_dalek_ng::ristretto::CompressedRistretto as ExternalCompressedRistrettoPoint;
 use curve25519_dalek_ng::ristretto::RistrettoPoint as ExternalRistrettoPoint;
 use curve25519_dalek_ng::scalar::Scalar as ExternalRistrettoScalar;
-use curve25519_dalek_ng::traits::{Identity, VartimeMultiscalarMul};
+use curve25519_dalek_ng::traits::{Identity, MultiscalarMul};
 use derive_more::{Add, Div, From, Neg, Sub};
 use fastcrypto_derive::GroupOpsExtend;
 use serde::{de, Deserialize};
@@ -67,12 +67,10 @@ impl RistrettoPoint {
             return Err(FastCryptoError::InvalidInput);
         }
 
-        ExternalRistrettoPoint::optional_multiscalar_mul(
+        Ok(RistrettoPoint(ExternalRistrettoPoint::multiscalar_mul(
             scalars_iter.map(|s| s.into().0),
-            points_iter.map(|g| Option::Some(g.0)),
-        )
-        .map(RistrettoPoint)
-        .ok_or(FastCryptoError::GeneralError)
+            points_iter.map(|g| g.0),
+        )))
     }
 }
 
