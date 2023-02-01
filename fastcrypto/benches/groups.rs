6f0c19a413ec1027d9a06f2e3d143f25449015c7
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-02-01 12:02:41 +0100

    Add group ops benchmarks

---
+++ fastcrypto/benches/groups.rs
@@ -0,0 +1,90 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+#[macro_use]
+extern crate criterion;
+
+mod group_benches {
+    use criterion::{measurement, BenchmarkGroup, Criterion};
+    use fastcrypto::groups::bls12381::{G1Element, G2Element, GTElement};
+    use fastcrypto::groups::ristretto255::RistrettoPoint;
+    use fastcrypto::groups::{GroupElement, HashToGroupElement, Pairing, Scalar};
+    use rand::thread_rng;
+
+    fn add_single<G: GroupElement, M: measurement::Measurement>(
+        name: &str,
+        c: &mut BenchmarkGroup<M>,
+    ) {
+        let x = G::generator() * G::ScalarType::rand(&mut thread_rng());
+        let y = G::generator() * G::ScalarType::rand(&mut thread_rng());
+        c.bench_function(&(name.to_string()), move |b| b.iter(|| x + y));
+    }
+
+    fn add(c: &mut Criterion) {
+        let mut group: BenchmarkGroup<_> = c.benchmark_group("Add");
+        add_single::<G1Element, _>("BLS12381-G1", &mut group);
+        add_single::<G2Element, _>("BLS12381-G2", &mut group);
+        add_single::<GTElement, _>("BLS12381-GT", &mut group);
+        add_single::<RistrettoPoint, _>("Ristretto255", &mut group);
+    }
+
+    fn scale_single<G: GroupElement, M: measurement::Measurement>(
+        name: &str,
+        c: &mut BenchmarkGroup<M>,
+    ) {
+        let x = G::generator() * G::ScalarType::rand(&mut thread_rng());
+        let y = G::ScalarType::rand(&mut thread_rng());
+        c.bench_function(&(name.to_string()), move |b| b.iter(|| x * y));
+    }
+
+    fn scale(c: &mut Criterion) {
+        let mut group: BenchmarkGroup<_> = c.benchmark_group("Scale");
+        scale_single::<G1Element, _>("BLS12381-G1", &mut group);
+        scale_single::<G2Element, _>("BLS12381-G2", &mut group);
+        scale_single::<GTElement, _>("BLS12381-GT", &mut group);
+        scale_single::<RistrettoPoint, _>("Ristretto255", &mut group);
+    }
+
+    fn hash_to_group_single<G: GroupElement + HashToGroupElement, M: measurement::Measurement>(
+        name: &str,
+        c: &mut BenchmarkGroup<M>,
+    ) {
+        let seed = b"Hello, World!";
+        c.bench_function(&(name.to_string()), move |b| {
+            b.iter(|| G::hash_to_group_element(seed))
+        });
+    }
+
+    fn hash_to_group(c: &mut Criterion) {
+        let mut group: BenchmarkGroup<_> = c.benchmark_group("Hash-to-group");
+        hash_to_group_single::<G1Element, _>("BLS12381-G1", &mut group);
+        hash_to_group_single::<G2Element, _>("BLS12381-G2", &mut group);
+        hash_to_group_single::<RistrettoPoint, _>("Ristretto255", &mut group);
+    }
+
+    fn pairing_single<G: GroupElement + Pairing, M: measurement::Measurement>(
+        name: &str,
+        c: &mut BenchmarkGroup<M>,
+    ) {
+        let x = G::generator() * G::ScalarType::rand(&mut thread_rng());
+        let y = G::Other::generator()
+            * <<G as Pairing>::Other as GroupElement>::ScalarType::rand(&mut thread_rng());
+        c.bench_function(&(name.to_string()), move |b| b.iter(|| G::pairing(&x, &y)));
+    }
+
+    fn pairing(c: &mut Criterion) {
+        let mut group: BenchmarkGroup<_> = c.benchmark_group("Pairing");
+        pairing_single::<G1Element, _>("BLS12381-G1", &mut group);
+    }
+
+    criterion_group! {
+        name = group_benches;
+        config = Criterion::default().sample_size(100);
+        targets =
+            add,
+            scale,
+            hash_to_group,
+            pairing,
+    }
+}
+
+criterion_main!(group_benches::group_benches,);
