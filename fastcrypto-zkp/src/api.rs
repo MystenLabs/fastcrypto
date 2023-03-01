e03f82c8b665f97d568487d9500e482c5a5320b6
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-03-01 20:38:57 +0100

    Allow multiple public inputs in verify api

--- fastcrypto-zkp/src/api.rs
+++ fastcrypto-zkp/src/api.rs
@@ -5,6 +5,7 @@ use ark_bls12_381::Bls12_381;
 use ark_groth16::{Proof, VerifyingKey};
 use ark_serialize::CanonicalDeserialize;
 use fastcrypto::error::FastCryptoError;
+use fastcrypto::groups::bls12381::SCALAR_LENGTH;
 
 use crate::conversions::BlsFr;
 use crate::verifier::{process_vk_special, verify_with_processed_vk, PreparedVerifyingKey};
@@ -22,7 +23,8 @@ pub fn prepare_pvk_bytes(vk_bytes: &[u8]) -> Result<Vec<Vec<u8>>, FastCryptoErro
 }
 
 /// Verify Groth16 proof using the serialized form of the four components in a prepared verifying key
-/// (see more at [`crate::verifier::PreparedVerifyingKey`]), serialized proof public input and serialized proof points.
+/// (see more at [`crate::verifier::PreparedVerifyingKey`]), serialized proof public input, which should
+/// be serialized field elements of the scalar field, and serialized proof points.
 pub fn verify_groth16_in_bytes(
     vk_gamma_abc_g1_bytes: &[u8],
     alpha_g1_beta_g2_bytes: &[u8],
@@ -31,8 +33,13 @@ pub fn verify_groth16_in_bytes(
     proof_public_inputs_as_bytes: &[u8],
     proof_points_as_bytes: &[u8],
 ) -> Result<bool, FastCryptoError> {
-    let x = BlsFr::deserialize_compressed(proof_public_inputs_as_bytes)
-        .map_err(|_| FastCryptoError::InvalidInput)?;
+    let mut x = Vec::new();
+    for chunk in proof_public_inputs_as_bytes.chunks(SCALAR_LENGTH) {
+        if chunk.len() != SCALAR_LENGTH {
+            return Err(FastCryptoError::InputLengthWrong(SCALAR_LENGTH));
+        }
+        x.push(BlsFr::deserialize_compressed(chunk).map_err(|_| FastCryptoError::InvalidInput)?);
+    }
 
     let proof = Proof::<Bls12_381>::deserialize_compressed(proof_points_as_bytes)
         .map_err(|_| FastCryptoError::InvalidInput)?;
@@ -44,6 +51,6 @@ pub fn verify_groth16_in_bytes(
         delta_g2_neg_pc_bytes,
     )?;
 
-    verify_with_processed_vk(&blst_pvk, &[x], &proof)
+    verify_with_processed_vk(&blst_pvk, &x, &proof)
         .map_err(|_| FastCryptoError::GeneralOpaqueError)
 }
