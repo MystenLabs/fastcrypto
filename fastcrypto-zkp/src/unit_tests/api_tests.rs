3fce98e426fac6d7044dc539fe8cd80b59739c0d
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-03-02 09:26:24 +0100

    License

--- fastcrypto-zkp/src/unit_tests/api_tests.rs
+++ fastcrypto-zkp/src/unit_tests/api_tests.rs
@@ -112,3 +112,49 @@ fn test_prepare_pvk_bytes() {
     modified_bytes.pop();
     assert!(prepare_pvk_bytes(&modified_bytes).is_err());
 }
+
+#[test]
+fn test_verify_groth16_in_bytes_multiple_inputs() {
+    let mut rng = thread_rng();
+
+    let a = Fr::from(123);
+    let b = Fr::from(456);
+
+    let params = {
+        let circuit = Fibonacci::<Fr>::new(42, a, b); // 42 constraints, initial a = b = 1
+        generate_random_parameters::<Bls12_381, _, _>(circuit, &mut rng).unwrap()
+    };
+
+    let pvk = process_vk_special(&params.vk);
+
+    let proof = {
+        let circuit = Fibonacci::<Fr>::new(42, a, b); // 42 constraints, initial a = b = 1
+        create_random_proof(circuit, &params, &mut rng).unwrap()
+    };
+
+    let inputs: Vec<_> = [a, b].to_vec();
+    assert!(verify_with_processed_vk(&pvk, &inputs, &proof).unwrap());
+
+    let pvk = pvk.as_serialized().unwrap();
+
+    let mut inputs_bytes = Vec::new();
+    a.serialize(&mut inputs_bytes).unwrap();
+    b.serialize(&mut inputs_bytes).unwrap();
+
+    // Proof::write serializes uncompressed and also adds a length to each element, so we serialize
+    // each individual element here to avoid that.
+    let mut proof_bytes = Vec::new();
+    proof.a.serialize(&mut proof_bytes).unwrap();
+    proof.b.serialize(&mut proof_bytes).unwrap();
+    proof.c.serialize(&mut proof_bytes).unwrap();
+
+    assert!(verify_groth16_in_bytes(
+        &pvk[0],
+        &pvk[1],
+        &pvk[2],
+        &pvk[3],
+        &inputs_bytes,
+        &proof_bytes
+    )
+        .unwrap());
+}
