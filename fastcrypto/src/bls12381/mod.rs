MystenLabs/fastcrypto

commit 5454b7f65632b35cb9503fb14ac0aca3823b5706

Re-revert batch_verify impl and allow empty batches (#487)
* Allow empty batches

* Sig groupcheck

fastcrypto/src/bls12381/mod.rs
@@ -259,7 +259,7 @@ impl VerifyingKey for BLS12381PublicKey {
             &pks.iter().map(|pk| &pk.pubkey).collect::<Vec<_>>(),
             false,
             &sigs.iter().map(|sig| &sig.sig).collect::<Vec<_>>(),
-            false,
+            true,
             &rands,
             BLS_BATCH_RANDOM_SCALAR_LENGTH,
         );
@@ -682,25 +682,34 @@ impl AggregateAuthenticator for BLS12381AggregateSignature {
         if signatures.len() != pks.len() || signatures.len() != messages.len() {
             return Err(FastCryptoError::InputLengthWrong(signatures.len()));
         }
-        let mut pk_iter = pks.into_iter();
-        for i in 0..signatures.len() {
-            let sig = signatures[i].sig;
-            let result = sig
-                .fast_aggregate_verify(
-                    true,
-                    messages[i],
-                    $dst_string,
-                    &pk_iter
-                        .next()
-                        .unwrap()
-                        .map(|x| &x.pubkey)
-                        .collect::<Vec<_>>()[..],
-                );
-            if result != BLST_ERROR::BLST_SUCCESS {
-                return Err(FastCryptoError::GeneralOpaqueError);
-            }
+
+        if signatures.is_empty() {
+            // verify_multiple_aggregate_signatures fails on empty input, but we accept here.
+            return Ok(())
         }
-        Ok(())
+
+        let mut agg_pks: Vec<blst::PublicKey> = Vec::with_capacity(signatures.len());
+        for keys in pks {
+             let keys_as_vec = keys.map(|x| x.pubkey.borrow()).collect::<Vec<_>>();
+             agg_pks.push(blst::AggregatePublicKey::aggregate(&keys_as_vec, false).unwrap().to_public_key()
+             );
+         }
+
+        let result = blst::Signature::verify_multiple_aggregate_signatures(
+            &messages,
+            $dst_string,
+            &agg_pks.iter().map(|m| m.borrow()).collect::<Vec<_>>(),
+            false,
+            &signatures.iter().map(|agg_sig| &agg_sig.sig).collect::<Vec<_>>(),
+            false,
+            &get_random_scalars(signatures.len()),
+            BLS_BATCH_RANDOM_SCALAR_LENGTH,
+        );
+        if result == BLST_ERROR::BLST_SUCCESS {
+            Ok(())
+        } else {
+            Err(FastCryptoError::GeneralOpaqueError)
+        }
     }
 }
 

commit 3f9128b85f8b144f7a5cbf79d14218d8f3a5edbb

Check sigs in verify_multiple_aggregate_signatures
Fix a bug introduced in https://github.com/MystenLabs/fastcrypto/commit/5454b7f65632b35cb9503fb14ac0aca3823b5706

fastcrypto/src/bls12381/mod.rs
@@ -705,7 +705,7 @@ impl AggregateAuthenticator for BLS12381AggregateSignature {
             &agg_pks.iter().map(|m| m.borrow()).collect::<Vec<_>>(),
             false,
             &signatures.iter().map(|agg_sig| &agg_sig.sig).collect::<Vec<_>>(),
-            false,
+            true,
             &get_random_scalars(signatures.len()),
             BLS_BATCH_RANDOM_SCALAR_LENGTH,
         );