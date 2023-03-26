db8d5ab49fbfcf081f08f1aac007d63d757f6b60
Nicolas Schapeler <nicoschap@live.fr>
2023-03-26 22:46:49 +0200

    fix clippy warnings

--- fastcrypto-zkp/benches/utils.rs
+++ fastcrypto-zkp/benches/utils.rs
@@ -1,7 +1,13 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use ark_bn254::{Fq, Fq2, G1Affine, G1Projective, G2Affine};
 
+type StrPair = (&'static str, &'static str);
+type StrTriplet = (&'static str, &'static str, &'static str);
+
 #[allow(non_snake_case)]
-pub fn G1Affine_from_str_projective(s: (&str, &str, &str)) -> G1Affine {
+pub fn G1Affine_from_str_projective(#[allow(clippy::type_complexity)] s: StrTriplet) -> G1Affine {
     G1Projective::new(
         s.0.parse::<Fq>().unwrap(),
         s.1.parse::<Fq>().unwrap(),
@@ -11,7 +17,7 @@ pub fn G1Affine_from_str_projective(s: (&str, &str, &str)) -> G1Affine {
 }
 
 #[allow(non_snake_case)]
-pub fn G2Affine_from_str_projective(s: ((&str, &str), (&str, &str), (&str, &str))) -> G2Affine {
+pub fn G2Affine_from_str_projective(s: (StrPair, StrPair, StrPair)) -> G2Affine {
     use ark_bn254::G2Projective;
     G2Projective::new(
         Fq2::new(s.0 .0.parse::<Fq>().unwrap(), s.0 .1.parse::<Fq>().unwrap()),
