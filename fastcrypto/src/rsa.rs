a118bae52ba6587d6c62686f78a09da989d78b12
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-03-28 10:37:17 +0200

    Remove unused methods

--- fastcrypto/src/rsa.rs
+++ fastcrypto/src/rsa.rs
@@ -4,6 +4,7 @@
 //! Implementation of RSASSA-PKCS1-v1_5.
 
 use crate::error::{FastCryptoError, FastCryptoResult};
+use crate::hash::{HashFunction, Sha256};
 use rsa::pkcs1::DecodeRsaPublicKey;
 use rsa::pkcs1v15::Signature as ExternalSignature;
 use rsa::pkcs8::{DecodePublicKey, EncodePublicKey};
@@ -17,6 +18,7 @@ pub struct RSAPublicKey(pub ExternalPublicKey);
 pub struct RSASignature(pub ExternalSignature);
 
 impl RSAPublicKey {
+    /// Parse an `RSAPublicKey` from a ASN.1 DER (Distinguished Encoding Rules) encoding.
     pub fn from_der(der: &[u8]) -> FastCryptoResult<Self> {
         // First try to parse the public key using PKCS#8 format and if this fails, try PKCS#1 format
         Ok(RSAPublicKey(
@@ -26,6 +28,8 @@ impl RSAPublicKey {
         ))
     }
 
+    /// Parse an `RSAPublicKey` from a PEM (Privacy-Enhanced Mail) encoding. Both PKCS#1 and PKCS#8
+    /// formats are supported.
     pub fn from_pem(pem: &str) -> FastCryptoResult<Self> {
         // First try to parse the public key using PKCS#8 format and if this fails, try PKCS#1 format
         let pem = pem.trim();
@@ -36,26 +40,17 @@ impl RSAPublicKey {
         ))
     }
 
-    pub fn to_der(&self) -> Result<Vec<u8>, FastCryptoError> {
-        self.0
-            .to_public_key_der()
-            .map_err(|_| {
-                FastCryptoError::GeneralError("Unable to export invalid public key".to_string())
-            })
-            .map(|x| x.as_ref().to_vec())
+    /// Verify a signed message. The verification uses SHA-256 for hashing.
+    pub fn verify(&self, msg: &[u8], signature: &RSASignature) -> FastCryptoResult<()> {
+        self.verify_prehash(&Sha256::digest(msg).digest, signature)
     }
 
-    pub fn to_pem(&self) -> Result<String, FastCryptoError> {
-        self.0.to_public_key_pem(Default::default()).map_err(|_| {
-            FastCryptoError::GeneralError("Unable to export invalid public key".to_string())
-        })
-    }
-
-    pub fn verify_prehash(&self, msg: &[u8], signature: &RSASignature) -> FastCryptoResult<()> {
+    /// Verify a signed message. The message, `hashed`, must be the output of a cryptographic hash function.
+    pub fn verify_prehash(&self, hashed: &[u8], signature: &RSASignature) -> FastCryptoResult<()> {
         self.0
             .verify(
                 Pkcs1v15Sign::new::<sha2::Sha256>(),
-                msg,
+                hashed,
                 signature.0.as_ref(),
             )
             .map_err(|_| FastCryptoError::InvalidSignature)
@@ -86,15 +81,12 @@ mod test {
         let pk = RSAPublicKey::from_pem(pk_pem).unwrap();
 
         let header_and_payload = b"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjE0ZWJjMDRlNmFjM2QzZTk2MDMxZDJjY2QzODZmY2E5NWRkZjMyZGQifQ.eyJpc3MiOiJodHRwczovL3d3dy5mYWNlYm9vay5jb20iLCJhdWQiOiIxMjQxNTU5MzY2NzU1MjE0Iiwic3ViIjoiNzA4NTYyNjExMDA5NTI1IiwiaWF0IjoxNjc5OTMyMDE0LCJleHAiOjE2Nzk5MzU2MTQsImp0aSI6IlRLdnouZGJlYzdjYTMxOTQyYTVkMmU1NmJkMGRiZmI4MjRiMTcxODVlMGYzMGIyMGYyNTczZGU1ZDQ4ZmM5ZjU4M2U0MyIsIm5vbmNlIjoidGVzdCIsImdpdmVuX25hbWUiOiJKb3kiLCJmYW1pbHlfbmFtZSI6IldhbmciLCJuYW1lIjoiSm95IFdhbmciLCJwaWN0dXJlIjoiaHR0cHM6Ly9wbGF0Zm9ybS1sb29rYXNpZGUuZmJzYnguY29tL3BsYXRmb3JtL3Byb2ZpbGVwaWMvP2FzaWQ9NzA4NTYyNjExMDA5NTI1JmhlaWdodD0xMDAmd2lkdGg9MTAwJmV4dD0xNjgyNTI0MDE1Jmhhc2g9QWVTMENxblhPMmNhT3g4WDhRZyJ9";
-        let msg = Sha256::digest(header_and_payload).digest;
 
         let signature = "Z65bdJv-sFO9mNe4i1Tv0fa74rEtSIh3ZzJ29JtojgpA_d40JfE_NVJliYvoZdfqPX85a8NAG-ujKWWzrtv8l3K33r-T0WuUvosai99Y7TrMZt3WtT9pLwoO4s8KPSr9jXjTD94YFhizdKtyHFvaJRVjyUWFTvsQgZP9kyiSPh-7R_CStVan2u0scZRosZeOlZT4dI5xXnt3AFH-vFfaWiZEEunKljIkqvdrtt3x-HLFnjSvKGFi1Ct4LBObdjbNGJULYjQ0-N7yuQevaiYEpSFW1NBfa3p52vMj9XMADhg4wrV7Nuvk7CqERLeL-M8L_KmUGnRXOmMUL-6KTC8Rtw";
         let signature_bytes = Base64UrlUnpadded::decode_vec(signature).unwrap();
         let signature = RSASignature::from_bytes(&signature_bytes).unwrap();
 
-        assert!(pk
-            .verify_prehash(msg.as_slice(), &signature)
-            .is_ok());
+        assert!(pk.verify(header_and_payload, &signature).is_ok());
     }
 
     // TODO: This currently fails at a test case with a short signature. Need to investigate why.
