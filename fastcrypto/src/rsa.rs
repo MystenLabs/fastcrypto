496fcb5083c7a7203423c4a56f36a567590f6b52
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-03-29 11:16:20 +0200

    Public key from parts and new test vector

--- fastcrypto/src/rsa.rs
+++ fastcrypto/src/rsa.rs
@@ -7,7 +7,7 @@ use crate::error::{FastCryptoError, FastCryptoResult};
 use crate::hash::{HashFunction, Sha256};
 use rsa::pkcs1::DecodeRsaPublicKey;
 use rsa::pkcs1v15::Signature as ExternalSignature;
-use rsa::RsaPublicKey as ExternalPublicKey;
+use rsa::{BigUint, RsaPublicKey as ExternalPublicKey};
 use rsa::{Pkcs1v15Sign, PublicKey};
 
 #[derive(Clone)]
@@ -17,10 +17,22 @@ pub struct RSAPublicKey(pub ExternalPublicKey);
 pub struct RSASignature(pub ExternalSignature);
 
 impl RSAPublicKey {
-    /// Parse an `RSAPublicKey` from a ASN.1 DER (Distinguished Encoding Rules) encoding according to PKCS #1.
+    /// Parse an `RSAPublicKey` from an ASN.1 DER (Distinguished Encoding Rules) PKCS #1 encoding.
     pub fn from_der(der: &[u8]) -> FastCryptoResult<Self> {
         Ok(RSAPublicKey(
             rsa::RsaPublicKey::from_pkcs1_der(der).map_err(|_| FastCryptoError::InvalidInput)?,
+        ))
+    }
+
+    /// Parse an `RSAPublicKey` from its components, eg. the modulus (n) and the exponent (e) from a binary big-endian representation.
+    pub fn from_raw_components(modulus: &[u8], exponent: &[u8]) -> FastCryptoResult<Self> {
+        // The Base64 encodings in a JSON Web Key is big-endian encoded (see RFC 7517 and 7518), so we expect the same here.
+        Ok(RSAPublicKey(
+            rsa::RsaPublicKey::new(
+                BigUint::from_bytes_be(modulus),
+                BigUint::from_bytes_be(exponent),
+            )
+            .map_err(|_| FastCryptoError::InvalidInput)?,
         ))
     }
 
@@ -58,28 +70,37 @@ mod test {
 
     #[test]
     fn jwt_test() {
-        // Test vector generated with https://dinochiesa.github.io/jwt/ and signed with RS256.
-        let pk_der = &hex::decode("3082010a0282010100e4d5c60d77dbd450ee5a003140f547a3e0cf524165f2b0a37e3d27bd0fbe8ae6df32c3293f7518bb6dbd1b061ea4eb4a3a96b8240eae6065626d7aa5e65761559286e0bac63bc4cbdd2e42daaa9a7350906e1c321be0eec8a063d457b90bb2ff4d62a64dea7778a129a25857b9ac4d1d1e91e53b314dbf744454e85515ffdb4ae5f2d1f3feccd6c8fce69cb20ea9a7bb50254f1717a76f73bbc5af12bd2ad5acfb3611e8f778480e60e46c84fdc70ba390f0657ba2e26e33a5573dd4616fc044586fec4ca9a4afdaafaeda21d8fa4196711ecdab7eeed6dd74f748e8b078cf80884336244eeece159639455d3ae1cdec90c9a4121b77fd0adbefe310b04598970203010001").unwrap();
-        let pk = RSAPublicKey::from_der(pk_der).unwrap();
+        // Test vector from with RFC 7515 section A.2.1.
+        let n_base64 = "ofgWCuLjybRlzo0tZWJjNiuSfb4p4fAkd_wWJcyQoTbji9k0l8W26mPddxHmfHQp-Vaw-4qPCJrcS2mJPMEzP1Pt0Bm4d4QlL-yRT-SFd2lZS-pCgNMsD1W_YpRPEwOWvG6b32690r2jZ47soMZo9wGzjb_7OMg0LOL-bSf63kpaSHSXndS5z5rexMdbBYUsLA9e-KXBdQOS-UTo7WTBEMa2R2CapHg665xsmtdVMTBQY4uDZlxvb3qCo5ZwKh9kG4LT6_I5IhlJH7aGhyxXFvUK-DWNmoudF8NAco9_h9iaGNj8q2ethFkMLs91kzk2PAcDTW9gb54h4FRWyuXpoQ";
+        let n_bytes = Base64UrlUnpadded::decode_vec(n_base64).unwrap();
 
-        let msg = b"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjE0ZWJjMDRlNmFjM2QzZTk2MDMxZDJjY2QzODZmY2E5NWRkZjMyZGQifQ.eyJpc3MiOiJEaW5vQ2hpZXNhLmdpdGh1Yi5pbyIsInN1YiI6ImlkcmlzIiwiYXVkIjoiZXZhbmRlciIsImlhdCI6MTY3OTk5MzM5NywiZXhwIjoxNjc5OTkzOTk3LCJhbHBoYSI6NjgyfQ";
+        let e_base64 = "AQAB";
+        let e_bytes = Base64UrlUnpadded::decode_vec(e_base64).unwrap();
+
+        let pk = RSAPublicKey::from_raw_components(&n_bytes, &e_bytes).unwrap();
+
+        let msg = b"eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ";
         let digest = Sha256::digest(msg).digest;
 
-        let signature = "ae2yui9qVdRCEQWq76WoAuXWqb5sE-iZgYu1VP0z7mDY6jMBu-SddKz9Uh-yDzBo9erjJdd4PhadsIUY_mml1hmAbvcGo-IMayw6M-SOKDfMI73sw0U-twzH3DHW9pB2PNUNV3T67mFVL60eOmw7KwotpdQewQwJ_34xEOm5g2UjkFDsFzDydnFmxMMm8PCEjg8DGRhKAfzdtUV00B4KzESSKhwu8oRzEU7D6mbvco7I1TIl3IOKT9JK8y_775_W9Flk6lMildTOe08upqnJeT3dgnKgYKbFyN9IgW_pr7Htu-prtVPXqlfhWxFtqmAZxnKaOXo14oeWWvX05E-wLw";
-        let signature_bytes = Base64UrlUnpadded::decode_vec(signature).unwrap();
+        let signature_base64 = "cC4hiUPoj9Eetdgtv3hF80EGrhuB__dzERat0XF9g2VtQgr9PJbu3XOiZj5RZmh7AAuHIm4Bh-0Qc_lF5YKt_O8W2Fp5jujGbds9uJdbF9CUAr7t1dnZcAcQjbKBYNX4BAynRFdiuB--f_nZLgrnbyTyWzO75vRK5h6xBArLIARNPvkSjtQBMHlb1L07Qe7K0GarZRmB_eSN9383LcOLn6_dO--xi12jzDwusC-eOkHWEsqtFZESc6BfI7noOPqvhJ1phCnvWh6IeYI2w9QOYEUipUTI8np6LbgGY9Fs98rqVt5AXLIhWkWywlVmtVrBp0igcN_IoypGlUPQGe77Rw";
+        let signature_bytes = Base64UrlUnpadded::decode_vec(signature_base64).unwrap();
         let signature = RSASignature::from_bytes(&signature_bytes).unwrap();
 
+        // Valid signature
         assert!(pk.verify_prehash(&digest, &signature).is_ok());
         assert!(pk.verify(msg, &signature).is_ok());
 
+        // Invalid digest
         let mut other_digest = digest;
         other_digest[0] += 1;
         assert!(pk.verify_prehash(&other_digest, &signature).is_err());
 
+        // Invalid message
         let mut other_msg = *msg;
         other_msg[0] += 1;
         assert!(pk.verify(&other_msg, &signature).is_err());
 
+        // Invalid signature
         let mut other_signature_bytes = signature_bytes;
         other_signature_bytes[7] += 1;
         let other_signature = RSASignature::from_bytes(&other_signature_bytes).unwrap();
