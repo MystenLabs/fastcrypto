e209f6dab4d7c7c201bd9039e0c710c46680f839
arnab-roy <arnabr@gmail.com>
2023-04-05 11:30:26 -0700

    Adding some Packer tests.

--- openid-zkp-auth/test/misc.js
+++ openid-zkp-auth/test/misc.js
@@ -50,4 +50,59 @@ describe("Miscellaneous checks", () => {
         
         assert.sameOrderedMembers(utils.getWitnessArray(witness, cir_fixed.symbols, "main.out"), [2n, 3n]);
     });
+    
+    it("Checking Packer", async () => {
+        { // Case 0: input and output should be same
+            cir_fixed = await test.genMain(path.join(__dirname, "..", "circuits", "misc.circom"), "Packer", [4, 4, 4, 4]);
+            await cir_fixed.loadSymbols();
+            input = [1,2,3,4];
+            const witness = await cir_fixed.calculateWitness({ "in": input });
+            
+            const out = utils.getWitnessArray(witness, cir.symbols, "main.out").map(e => Number(e) - '0');
+            assert.deepEqual(out, [1, 2, 3, 4]);
+        }
+
+        { // Case 1: Output width is multiple of input width
+            cir_fixed = await test.genMain(path.join(__dirname, "..", "circuits", "misc.circom"), "Packer", [4, 4, 8, 2]);
+            await cir_fixed.loadSymbols();
+            input = [1,2,3,4];
+            const witness = await cir_fixed.calculateWitness({ "in": input });
+            
+            const out = utils.getWitnessArray(witness, cir.symbols, "main.out").map(e => Number(e) - '0');
+            assert.deepEqual(out[0], 18);
+            assert.deepEqual(out[1], 52);
+        }
+
+        { // Case 2: Output width is not a multiple of input width
+            cir_fixed = await test.genMain(path.join(__dirname, "..", "circuits", "misc.circom"), "Packer", [4, 4, 6, 3]);
+            await cir_fixed.loadSymbols();
+            input = [1,2,3,4];
+            const witness = await cir_fixed.calculateWitness({ "in": input });
+            
+            const out = utils.getWitnessArray(witness, cir.symbols, "main.out").map(e => Number(e) - '0');
+            assert.deepEqual(out[0], 4);
+            assert.deepEqual(out[1], 35);
+            assert.deepEqual(out[2], 16)
+        }
+
+        { // Case 3: Edge case - just one input
+            cir_fixed = await test.genMain(path.join(__dirname, "..", "circuits", "misc.circom"), "Packer", [1, 1, 6, 1]);
+            await cir_fixed.loadSymbols();
+            input = [1];
+            const witness = await cir_fixed.calculateWitness({ "in": input });
+            
+            const out = utils.getWitnessArray(witness, cir.symbols, "main.out").map(e => Number(e) - '0');
+            assert.deepEqual(out[0], 32);
+        }
+
+        { // Case 3: Edge case - just one output
+            cir_fixed = await test.genMain(path.join(__dirname, "..", "circuits", "misc.circom"), "Packer", [4, 4, 16, 1]);
+            await cir_fixed.loadSymbols();
+            input = [1, 2, 3, 4];
+            const witness = await cir_fixed.calculateWitness({ "in": input });
+            
+            const out = utils.getWitnessArray(witness, cir.symbols, "main.out").map(e => Number(e) - '0');
+            assert.deepEqual(out[0], 4660);
+        }
+    });
 })
