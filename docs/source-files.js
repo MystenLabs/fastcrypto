77f47f607c486cc3826fb47186b3339b1def2c9d
jonas-lj <jonas-lj@users.noreply.github.com>
2023-04-27 08:07:25 +0000

    deploy: b41c1b5581f907bebc9416e68072b72b5a13fa10

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,7 +1,7 @@
 var sourcesIndex = JSON.parse('{\
 "ecvrf_cli":["",[],["ecvrf.rs"]],\
 "encode_cli":["",[],["encode_cli.rs"]],\
-"fastcrypto":["",[["bls12381",[["min_pk",[],["mod.rs","mskr.rs"]],["min_sig",[],["mod.rs","mskr.rs"]]],["mod.rs","mskr.rs"]],["groups",[],["bls12381.rs","mod.rs","ristretto255.rs"]],["secp256k1",[],["mod.rs","recoverable.rs"]],["secp256r1",[],["mod.rs","recoverable.rs"]]],["aes.rs","bulletproofs.rs","ed25519.rs","encoding.rs","error.rs","hash.rs","hmac.rs","lib.rs","private_seed.rs","rsa.rs","serde_helpers.rs","signature_service.rs","traits.rs","vrf.rs"]],\
+"fastcrypto":["",[["bls12381",[["min_pk",[],["mod.rs","mskr.rs"]],["min_sig",[],["mod.rs","mskr.rs"]]],["mod.rs","mskr.rs"]],["groups",[],["bls12381.rs","mod.rs","ristretto255.rs"]],["secp256k1",[],["mod.rs","recoverable.rs"]],["secp256r1",[],["conversion.rs","mod.rs","recoverable.rs"]]],["aes.rs","bulletproofs.rs","ed25519.rs","encoding.rs","error.rs","hash.rs","hmac.rs","lib.rs","private_seed.rs","rsa.rs","serde_helpers.rs","signature_service.rs","traits.rs","vrf.rs"]],\
 "fastcrypto_derive":["",[],["lib.rs"]],\
 "fastcrypto_tbls":["",[],["lib.rs"]],\
 "fastcrypto_zkp":["",[["bls12381",[],["api.rs","conversions.rs","mod.rs","verifier.rs"]],["bn254",[],["api.rs","mod.rs","verifier.rs"]]],["dummy_circuits.rs","lib.rs"]],\
