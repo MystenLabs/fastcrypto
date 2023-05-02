c2d82fdb2e9f0e8cb4fcee8c615d52d0c7642c08
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-05-02 13:58:51 +0200

    Structure test cases

--- fastcrypto-cli/src/sigs_cli.rs
+++ fastcrypto-cli/src/sigs_cli.rs
@@ -295,36 +295,67 @@ mod tests {
     use crate::{execute, Command, KeygenArguments, SigningArguments, VerifiyingArguments};
     use fastcrypto::error::FastCryptoError;
 
+    struct TestParams {
+        name: &'static str,
+        private: &'static str,
+        public: &'static str,
+        sig: &'static str,
+    }
+
     // Test vectors
     const MSG: &str = "00010203";
     const SEED: &str = "0101010101010101010101010101010101010101010101010101010101010101";
-    const ED25519: &str = "ed25519";
-    const ED25519_SECRET_KEY: &str =
-        "3301e8d7e754db2cf57b0a4ca73f253c7053ad2bc5398777ba039b258e59ad9d";
-    const ED25519_PUBLIC_KEY: &str =
-        "8c553335eee80b9bfa0c544a45fe63474a09dff9c4b0b33db2b662f934ea46c4";
-    const ED25519_SIGNATURE: &str = "e929370aa36bef3a6b51594b6d96e0f389f09f28807e6b3a25d0ea93f56dd4659e15995f87545ab8f7f924bc18e0502fa689a57e57e931620b79a6c9ec7b3208";
-    const SECP256K1: &str = "secp256k1";
-    const SECP256K1_SECRET_KEY: &str =
-        "3301e8d7e754db2cf57b0a4ca73f253c7053ad2bc5398777ba039b258e59ad9d";
-    const SECP256K1_PUBLIC_KEY: &str =
-        "033e99a541db69bd32040dfe5037fbf5210dafa8151a71e21c5204b05d95ce0a62";
-    const SECP256K1_SIGNATURE: &str = "416a21d50b3c838328d4f03213f8ef0c3776389a972ba1ecd37b56243734eba208ea6aaa6fc076ad7accd71d355f693a6fe54fe69b3c168eace9803827bc9046";
-    const SECP256K1_RECOVERABLE: &str = "secp256k1-rec";
-    const SECP256K1_RECOVERABLE_SIGNATURE: &str = "416a21d50b3c838328d4f03213f8ef0c3776389a972ba1ecd37b56243734eba208ea6aaa6fc076ad7accd71d355f693a6fe54fe69b3c168eace9803827bc904601";
-    const SECP256R1: &str = "secp256r1";
-    const SECP256R1_SECRET_KEY: &str =
-        "3301e8d7e754db2cf57b0a4ca73f253c7053ad2bc5398777ba039b258e59ad9d";
-    const SECP256R1_PUBLIC_KEY: &str =
-        "035a8b075508c75f4a124749982a7d21f80d9a5f6893e41a9e955fe4c821e0debe";
-    const SECP256R1_SIGNATURE: &str = "54d7d68b43d65f718f3a92041292a514987739c36158a836b2218c505ba0e17c661642e58c996ba78f0cca493690b89658d0da3b9333a9e4fcea9ebf13da64bd";
-    const SECP256R1_RECOVERABLE: &str = "secp256r1-rec";
-    const SECP256R1_RECOVERABLE_SIGNATURE: &str = "54d7d68b43d65f718f3a92041292a514987739c36158a836b2218c505ba0e17c661642e58c996ba78f0cca493690b89658d0da3b9333a9e4fcea9ebf13da64bd01";
-    const BLS12381: &str = "bls12381";
-    const BLS12381_SECRET_KEY: &str =
-        "5fbaab9bd5ed88305581c2926a67ac56fd987ade7658335b1fa1acd258a6f337";
-    const BLS12381_PUBLIC_KEY: &str = "a57feae28362201f657ccf6cdaba629758beb0214942804d2c084967d76908fe46ce355e0e735bdde2705620c7cf4b3903177f62ba43ba39277d952d80afee4fdc439a3ce2ce6fd113196d7de7aff7d1683ed507a21e6920119c91980329925b";
-    const BLS12381_SIGNATURE: &str = "a09f9b16ac4cfeadfd4d69b940cf9ead098a7d9f0df0a11d07820cb1dbacda6e1d0631529b1070ec1d8eb29fbc76a807";
+
+    const ED25519_TEST: TestParams = TestParams {
+        name: "ed25519",
+        private: "3301e8d7e754db2cf57b0a4ca73f253c7053ad2bc5398777ba039b258e59ad9d",
+        public: "8c553335eee80b9bfa0c544a45fe63474a09dff9c4b0b33db2b662f934ea46c4",
+        sig: "e929370aa36bef3a6b51594b6d96e0f389f09f28807e6b3a25d0ea93f56dd4659e15995f87545ab8f7f924bc18e0502fa689a57e57e931620b79a6c9ec7b3208",
+    };
+
+    const SECP256K1_TEST: TestParams = TestParams {
+        name: "secp256k1",
+        private: "3301e8d7e754db2cf57b0a4ca73f253c7053ad2bc5398777ba039b258e59ad9d",
+        public: "033e99a541db69bd32040dfe5037fbf5210dafa8151a71e21c5204b05d95ce0a62",
+        sig: "416a21d50b3c838328d4f03213f8ef0c3776389a972ba1ecd37b56243734eba208ea6aaa6fc076ad7accd71d355f693a6fe54fe69b3c168eace9803827bc9046",
+    };
+
+    const SECP256K1_RECOVERABLE_TEST: TestParams = TestParams {
+        name: "secp256k1-rec",
+        private: SECP256K1_TEST.private,
+        public: SECP256K1_TEST.public,
+        sig: "416a21d50b3c838328d4f03213f8ef0c3776389a972ba1ecd37b56243734eba208ea6aaa6fc076ad7accd71d355f693a6fe54fe69b3c168eace9803827bc904601",
+    };
+
+    const SECP256R1_TEST: TestParams = TestParams {
+        name: "secp256r1",
+        private: "3301e8d7e754db2cf57b0a4ca73f253c7053ad2bc5398777ba039b258e59ad9d",
+        public: "035a8b075508c75f4a124749982a7d21f80d9a5f6893e41a9e955fe4c821e0debe",
+        sig: "54d7d68b43d65f718f3a92041292a514987739c36158a836b2218c505ba0e17c661642e58c996ba78f0cca493690b89658d0da3b9333a9e4fcea9ebf13da64bd",
+    };
+
+    const SECP256R1_RECOVERABLE_TEST: TestParams = TestParams {
+        name: "secp256r1-rec",
+        private: SECP256R1_TEST.private,
+        public: SECP256R1_TEST.public,
+        sig: "54d7d68b43d65f718f3a92041292a514987739c36158a836b2218c505ba0e17c661642e58c996ba78f0cca493690b89658d0da3b9333a9e4fcea9ebf13da64bd01",
+    };
+
+    const BLS12381_TEST: TestParams = TestParams {
+        name: "bls12381",
+        private: "5fbaab9bd5ed88305581c2926a67ac56fd987ade7658335b1fa1acd258a6f337",
+        public: "a57feae28362201f657ccf6cdaba629758beb0214942804d2c084967d76908fe46ce355e0e735bdde2705620c7cf4b3903177f62ba43ba39277d952d80afee4fdc439a3ce2ce6fd113196d7de7aff7d1683ed507a21e6920119c91980329925b",
+        sig: "a09f9b16ac4cfeadfd4d69b940cf9ead098a7d9f0df0a11d07820cb1dbacda6e1d0631529b1070ec1d8eb29fbc76a807",
+    };
+
+    const TEST_CASES: [TestParams; 6] = [
+        ED25519_TEST,
+        SECP256K1_TEST,
+        SECP256K1_RECOVERABLE_TEST,
+        SECP256R1_TEST,
+        SECP256R1_RECOVERABLE_TEST,
+        BLS12381_TEST,
+    ];
 
     fn test_keygen_single(scheme: &str, seed: &str) -> Result<(), FastCryptoError> {
         execute(Command::Keygen(KeygenArguments {
@@ -336,24 +367,18 @@ mod tests {
     #[test]
     fn test_keygen() {
         // Valid
-        assert!(test_keygen_single(ED25519, SEED).is_ok());
-        assert!(test_keygen_single(SECP256K1, SEED).is_ok());
-        assert!(test_keygen_single(SECP256K1_RECOVERABLE, SEED).is_ok());
-        assert!(test_keygen_single(SECP256R1, SEED).is_ok());
-        assert!(test_keygen_single(SECP256R1_RECOVERABLE, SEED).is_ok());
-        assert!(test_keygen_single(BLS12381, SEED).is_ok());
+        for test_case in TEST_CASES {
+            assert!(test_keygen_single(test_case.name, SEED).is_ok());
+        }
 
         // Unknown scheme
         assert!(test_keygen_single("unknown_scheme", SEED).is_err());
 
         // Invalid seed
         let invalid_seed = "invalid seed";
-        assert!(test_keygen_single(ED25519, invalid_seed).is_err());
-        assert!(test_keygen_single(SECP256K1, invalid_seed).is_err());
-        assert!(test_keygen_single(SECP256K1_RECOVERABLE, invalid_seed).is_err());
-        assert!(test_keygen_single(SECP256R1, invalid_seed).is_err());
-        assert!(test_keygen_single(SECP256R1_RECOVERABLE, invalid_seed).is_err());
-        assert!(test_keygen_single(BLS12381, invalid_seed).is_err());
+        for test_case in TEST_CASES {
+            assert!(test_keygen_single(test_case.name, invalid_seed).is_err());
+        }
     }
 
     fn test_sign_single(scheme: &str, msg: &str, secret_key: &str) -> Result<(), FastCryptoError> {
@@ -367,53 +392,22 @@ mod tests {
     #[test]
     fn test_sign() {
         // Valid
-        assert!(test_sign_single(ED25519, MSG, ED25519_SECRET_KEY).is_ok());
-        assert!(test_sign_single(SECP256K1, MSG, SECP256K1_SECRET_KEY).is_ok());
-        assert!(test_sign_single(SECP256K1_RECOVERABLE, MSG, SECP256K1_SECRET_KEY).is_ok());
-        assert!(test_sign_single(SECP256R1, MSG, SECP256R1_SECRET_KEY).is_ok());
-        assert!(test_sign_single(SECP256R1_RECOVERABLE, MSG, SECP256R1_SECRET_KEY).is_ok());
-        assert!(test_sign_single(BLS12381, MSG, BLS12381_SECRET_KEY).is_ok());
+        for test_case in TEST_CASES {
+            assert!(test_sign_single(test_case.name, MSG, test_case.private).is_ok());
+        }
 
         // Unknown scheme
-        assert!(test_sign_single("unknown_scheme", MSG, ED25519_SECRET_KEY).is_err());
+        assert!(test_sign_single("unknown_scheme", MSG, ED25519_TEST.private).is_err());
 
         // Invalid secret key
-        assert!(test_sign_single(
-            ED25519,
-            MSG,
-            &ED25519_SECRET_KEY[0..ED25519_SECRET_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_sign_single(
-            SECP256K1,
-            MSG,
-            &SECP256K1_SECRET_KEY[0..SECP256K1_SECRET_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_sign_single(
-            SECP256K1_RECOVERABLE,
-            MSG,
-            &SECP256K1_SECRET_KEY[0..SECP256K1_SECRET_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_sign_single(
-            SECP256R1,
-            MSG,
-            &SECP256R1_SECRET_KEY[0..SECP256R1_SECRET_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_sign_single(
-            SECP256R1_RECOVERABLE,
-            MSG,
-            &SECP256R1_SECRET_KEY[0..SECP256R1_SECRET_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_sign_single(
-            BLS12381,
-            MSG,
-            &BLS12381_SECRET_KEY[0..BLS12381_SECRET_KEY.len() - 1]
-        )
-        .is_err());
+        for test_case in TEST_CASES {
+            assert!(test_sign_single(
+                test_case.name,
+                MSG,
+                &test_case.private[0..ED25519_TEST.private.len() - 1]
+            )
+            .is_err());
+        }
     }
 
     fn test_verify_single(
@@ -433,121 +427,38 @@ mod tests {
     #[test]
     fn test_verify() {
         // Valid
-        assert!(test_verify_single(ED25519, MSG, ED25519_SIGNATURE, ED25519_PUBLIC_KEY).is_ok());
-        assert!(
-            test_verify_single(SECP256K1, MSG, SECP256K1_SIGNATURE, SECP256K1_PUBLIC_KEY).is_ok()
-        );
-        assert!(test_verify_single(
-            SECP256K1_RECOVERABLE,
-            MSG,
-            SECP256K1_RECOVERABLE_SIGNATURE,
-            SECP256K1_PUBLIC_KEY
-        )
-        .is_ok());
-        assert!(
-            test_verify_single(SECP256R1, MSG, SECP256R1_SIGNATURE, SECP256R1_PUBLIC_KEY).is_ok()
-        );
-        assert!(test_verify_single(
-            SECP256R1_RECOVERABLE,
-            MSG,
-            SECP256R1_RECOVERABLE_SIGNATURE,
-            SECP256R1_PUBLIC_KEY
-        )
-        .is_ok());
-        assert!(test_verify_single(BLS12381, MSG, BLS12381_SIGNATURE, BLS12381_PUBLIC_KEY).is_ok());
+        for test_case in TEST_CASES {
+            assert!(
+                test_verify_single(test_case.name, MSG, test_case.sig, test_case.public).is_ok()
+            );
+        }
 
         // Unknown scheme
         assert!(
-            test_verify_single("unknown scheme", MSG, ED25519_SIGNATURE, ED25519_PUBLIC_KEY)
+            test_verify_single("unknown scheme", MSG, ED25519_TEST.sig, ED25519_TEST.public)
                 .is_err()
         );
 
         // Invalid signature (too short)
-        assert!(test_verify_single(
-            ED25519,
-            MSG,
-            &ED25519_SIGNATURE[..ED25519_SIGNATURE.len() - 1],
-            ED25519_PUBLIC_KEY
-        )
-        .is_err());
-        assert!(test_verify_single(
-            SECP256K1,
-            MSG,
-            &SECP256K1_SIGNATURE[..SECP256K1_SIGNATURE.len() - 1],
-            SECP256K1_PUBLIC_KEY
-        )
-        .is_err());
-        assert!(test_verify_single(
-            SECP256K1_RECOVERABLE,
-            MSG,
-            &SECP256K1_RECOVERABLE_SIGNATURE[..SECP256K1_RECOVERABLE_SIGNATURE.len() - 1],
-            SECP256K1_PUBLIC_KEY
-        )
-        .is_err());
-        assert!(test_verify_single(
-            SECP256R1,
-            MSG,
-            &SECP256R1_SIGNATURE[..SECP256R1_SIGNATURE.len() - 1],
-            SECP256R1_PUBLIC_KEY
-        )
-        .is_err());
-        assert!(test_verify_single(
-            SECP256R1_RECOVERABLE,
-            MSG,
-            &SECP256R1_RECOVERABLE_SIGNATURE[..SECP256R1_RECOVERABLE_SIGNATURE.len() - 1],
-            SECP256R1_PUBLIC_KEY
-        )
-        .is_err());
-        assert!(test_verify_single(
-            BLS12381,
-            MSG,
-            &BLS12381_SIGNATURE[..BLS12381_SIGNATURE.len() - 1],
-            BLS12381_PUBLIC_KEY
-        )
-        .is_err());
+        for test_case in TEST_CASES {
+            assert!(test_verify_single(
+                test_case.name,
+                MSG,
+                &test_case.sig[0..test_case.sig.len() - 1],
+                test_case.public
+            )
+            .is_err());
+        }
 
         // Invalid public key (too short)
-        assert!(test_verify_single(
-            ED25519,
-            MSG,
-            ED25519_SIGNATURE,
-            &ED25519_PUBLIC_KEY[..ED25519_PUBLIC_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_verify_single(
-            SECP256K1,
-            MSG,
-            SECP256K1_SIGNATURE,
-            &SECP256K1_PUBLIC_KEY[..SECP256K1_PUBLIC_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_verify_single(
-            SECP256K1_RECOVERABLE,
-            MSG,
-            SECP256K1_RECOVERABLE_SIGNATURE,
-            &SECP256K1_PUBLIC_KEY[..SECP256K1_PUBLIC_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_verify_single(
-            SECP256R1,
-            MSG,
-            SECP256R1_SIGNATURE,
-            &SECP256R1_PUBLIC_KEY[..SECP256R1_PUBLIC_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_verify_single(
-            SECP256R1_RECOVERABLE,
-            MSG,
-            SECP256R1_RECOVERABLE_SIGNATURE,
-            &SECP256R1_PUBLIC_KEY[..SECP256R1_PUBLIC_KEY.len() - 1]
-        )
-        .is_err());
-        assert!(test_verify_single(
-            BLS12381,
-            MSG,
-            BLS12381_SIGNATURE,
-            &BLS12381_PUBLIC_KEY[..BLS12381_PUBLIC_KEY.len() - 1]
-        )
-        .is_err());
+        for test_case in TEST_CASES {
+            assert!(test_verify_single(
+                test_case.name,
+                MSG,
+                test_case.sig,
+                &test_case.public[0..test_case.public.len() - 1]
+            )
+            .is_err());
+        }
     }
 }
