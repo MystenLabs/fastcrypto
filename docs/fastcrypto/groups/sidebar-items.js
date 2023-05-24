42706cda61caafeacadf2f368840998fd4e9d2d0
benr-ml <benr-ml@users.noreply.github.com>
2023-05-24 07:02:09 +0000

    deploy: 8a8a0c5893529bd1b6fb7524d793c72ceebf3106

--- docs/fastcrypto/groups/sidebar-items.js
+++ docs/fastcrypto/groups/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["bls12381","ristretto255"],"trait":["GroupElement","HashToGroupElement","Pairing","Scalar"]};
+window.SIDEBAR_ITEMS = {"mod":["bls12381","ristretto255"],"trait":["GroupElement","HashToGroupElement","MultiScalarMul","Pairing","Scalar"]};
