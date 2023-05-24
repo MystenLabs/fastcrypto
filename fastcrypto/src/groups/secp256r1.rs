9c256dfdf3b069368eae68a670ba33a2dde5ee2a
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-05-24 11:09:17 +0200

    Affine coordinates are not used

--- fastcrypto/src/groups/secp256r1.rs
+++ fastcrypto/src/groups/secp256r1.rs
@@ -8,16 +8,13 @@ use crate::error::FastCryptoError;
 use crate::groups::{Doubling, GroupElement, Scalar as ScalarTrait};
 use crate::serde_helpers::ToFromByteArray;
 use crate::traits::AllowedRng;
-use ark_ec::{CurveGroup, Group};
+use ark_ec::Group;
 use ark_ff::{Field, One, PrimeField, Zero};
-use ark_secp256r1::{Affine, Fr, Projective};
+use ark_secp256r1::{Fr, Projective};
 use ark_serialize::CanonicalSerialize;
 use derive_more::{Add, From, Neg, Sub};
 use fastcrypto_derive::GroupOpsExtend;
 use std::ops::{Div, Mul};
-
-/// A point on the Secp256r1 curve in affine coordinates.
-pub struct AffinePoint(Affine);
 
 /// A point on the Secp256r1 curve in projective coordinates.
 #[derive(Default, Clone, Copy, Debug, PartialEq, Eq, From, Add, Sub, Neg, GroupOpsExtend)]
@@ -26,12 +23,6 @@ pub struct ProjectivePoint(pub(crate) Projective);
 /// A field element in the prime field of the same order as the curve.
 #[derive(Default, Clone, Copy, Debug, PartialEq, Eq, From, Add, Sub, Neg, GroupOpsExtend)]
 pub struct Scalar(pub(crate) Fr);
-
-impl ProjectivePoint {
-    pub fn to_affine(&self) -> AffinePoint {
-        AffinePoint(self.0.into_affine())
-    }
-}
 
 impl Doubling for ProjectivePoint {
     fn double(&self) -> Self {
