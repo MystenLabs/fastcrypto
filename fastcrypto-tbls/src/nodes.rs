98c491af2abb5d4cb306d21152e2e9eeade9120c
benr-ml <benriva@mystenlabs.com>
2023-07-25 18:21:26 +0300

    License

--- fastcrypto-tbls/src/nodes.rs
+++ fastcrypto-tbls/src/nodes.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::ecies;
 use crate::types::ShareIndex;
 use fastcrypto::error::{FastCryptoError, FastCryptoResult};
