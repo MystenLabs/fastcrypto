d801b0421adc29a97b2dd7b0c0e469df7dc2ec52
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-08-09 14:07:05 +0200

    Add quadratic form composition algorithm

--- fastcrypto/src/groups/class_group/mod.rs
+++ fastcrypto/src/groups/class_group/mod.rs
@@ -38,8 +38,123 @@ impl Add<QuadraticForm> for QuadraticForm {
     type Output = QuadraticForm;
 
     fn add(self, rhs: QuadraticForm) -> Self::Output {
-        QuadraticForm {
-            form: self.form.compose(&rhs.form).reduce(),
+        // Slightly optimised version of Algorithm 1 from Jacobson, Jr, Michael & Poorten, Alfred
+        // (2002). "Computational aspects of NUCOMP", Lecture Notes in Computer Science.
+        // (https://www.researchgate.net/publication/221451638_Computational_aspects_of_NUCOMP)
+        // The paragraph numbers and variable names follow the paper.
+
+        let BinaryQF {
+            a: u1,
+            b: v1,
+            c: w1,
+        } = &self.form;
+        let BinaryQF {
+            a: u2,
+            b: v2,
+            c: w2,
+        } = &rhs.form;
+
+        // 1.
+        if w1 < w2 {
+            return rhs + self;
+        }
+        let s = (v1 + v2) >> 1;
+        let m = v2 - &s;
+
+        // 2.
+        let xgcd = BigInt::extended_gcd(&u2, &u1);
+        let f = xgcd.gcd;
+        let b = xgcd.x;
+        let c = xgcd.y;
+
+        let g: BigInt;
+        let capital_bx: BigInt;
+        let capital_by: BigInt;
+        let capital_cy: BigInt;
+        let capital_dy: BigInt;
+
+        if s.is_multiple_of(&f) {
+            g = f;
+            capital_bx = &m * &b;
+            capital_by = u1 / &g;
+            capital_cy = u2 / &g;
+            capital_dy = &s / &g;
+        } else {
+            // 3.
+            let xgcd = BigInt::extended_gcd(&f, &s);
+            g = xgcd.gcd;
+            let y = xgcd.y;
+            let h = &f / &g;
+            capital_by = u1 / &g;
+            capital_cy = u2 / &g;
+            capital_dy = &s / &g;
+
+            // 4.
+            let l = (&y * (&b * (w1.modulus(&h)) + &c * (w2.modulus(&h)))).modulus(&h);
+            capital_bx = &b * (&m / &h) + &l * (&capital_by / &h);
+        }
+
+        let mut bx = capital_bx.modulus(&capital_by);
+        let mut by = capital_by.clone();
+
+        let mut x = BigInt::one();
+        let mut y = BigInt::zero();
+        let mut z = 0u32;
+
+        while &by.abs() > &self.partial_gcd_limit && !bx.is_zero() {
+            let (q, mut t) = by.div_rem(&bx);
+            by = bx;
+            bx = t;
+            t = &y - &q * &x;
+            y = x;
+            x = t;
+            z += 1;
+        }
+
+        if z.is_odd() {
+            by = -by;
+            y = -y;
+        }
+
+        let ax = &g * &x;
+        let ay = &g * &y;
+
+        let u3: BigInt;
+        let w3: BigInt;
+        let v3: BigInt;
+
+        if z.is_zero() {
+            let q = &capital_cy * &bx;
+            let cx = (&q - &m) / &capital_by;
+            let dx = (&bx * &capital_dy - w2) / &capital_by;
+            u3 = &by * &capital_cy;
+            w3 = &bx * &cx - &g * &dx;
+            v3 = v2 - BigInt::from(2) * &q;
+        } else {
+            let cx = (&capital_cy * &bx - &m * &x) / &capital_by;
+            let q1 = &by * &cx;
+            let q2 = &q1 + &m;
+            let dx = (&capital_dy * &bx - w2 * &x) / &capital_by;
+            let q3 = &y * &dx;
+            let q4 = &q3 + &capital_dy;
+            let dy = &q4 / &x;
+            let cy = if !b.is_zero() {
+                &q2 / &bx
+            } else {
+                (&cx * &dy - w1) / &dx
+            };
+            u3 = &by * &cy - &ay * &dy;
+            w3 = &bx * &cx - &ax * &dx;
+            v3 = &g * (&q3 + &q4) - &q1 - &q2;
+        }
+
+        Self {
+            form: BinaryQF {
+                a: u3,
+                b: v3,
+                c: w3,
+            }
+            .reduce(),
             partial_gcd_limit: self.partial_gcd_limit,
         }
     }
@@ -82,7 +197,7 @@ impl ParameterizedGroupElement for QuadraticForm {
     }
 
     fn double(&self) -> Self {
-        // Slightly optimised version of algorithm 2 from Jacobson, Jr, Michael & Poorten, Alfred
+        // Slightly optimised version of Algorithm 2 from Jacobson, Jr, Michael & Poorten, Alfred
         // (2002). "Computational aspects of NUCOMP", Lecture Notes in Computer Science.
         // (https://www.researchgate.net/publication/221451638_Computational_aspects_of_NUCOMP)
         // The paragraph numbers and variable names follow the paper.
@@ -215,9 +330,30 @@ fn test_double() {
 
     let mut x = QuadraticForm::generator(&d);
     let mut y = QuadraticForm::generator(&d).form;
+    let mut z = QuadraticForm::generator(&d);
     for _ in 0..iterations {
         x = x.double();
         y = y.exp(&BigInt::from(2));
+        z = z.clone() + z;
         assert_eq!(x.form, y);
+        assert_eq!(z.form, y);
+    }
+}
+
+#[test]
+fn test_add() {
+    let d = Discriminant::try_from(BigInt::from(-1255)).unwrap();
+
+    unsafe {
+        pari_init(1000000, 0);
+    }
+    let iterations = 2;
+
+    let mut sum1 = QuadraticForm::generator(&d);
+    let mut sum2 = QuadraticForm::generator(&d).form;
+    for _ in 0..iterations {
+        sum1 = sum1 + QuadraticForm::generator(&d);
+        sum2 = sum2.compose(&QuadraticForm::generator(&d).form).reduce();
+        assert_eq!(sum1.form, sum2);
     }
 }
