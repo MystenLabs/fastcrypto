1da23b60a9da89b2720688d2f681f41704038b98
benr-ml <benriva@mystenlabs.com>
2023-10-01 19:14:06 +0300

    update RO

--- fastcrypto-tbls/src/random_oracle.rs
+++ fastcrypto-tbls/src/random_oracle.rs
@@ -5,9 +5,9 @@ use digest::Digest;
 use serde::{Deserialize, Serialize};
 use sha3::Sha3_512;
 
-/// Random Oracle from Sha3.
+/// Random Oracle from SHA3-512.
 /// - prefix should be globally unique.
-/// - evaluate serializes the given input and outputs SHA3-512(prefix_len as u32 | prefix | input).
+/// - evaluate serializes the given input and outputs SHA3-512(prefix_len as u8 | prefix | input).
 /// - Subprotocols may use a prefix that is extended from the prefix of its parent protocol, by
 ///   deriving a new instance using extend, which simply concatenates the strings with the separator
 ///   "-". The caller must make sure to choose distinct prefix & extension strings.
@@ -21,7 +21,7 @@ pub struct RandomOracle {
 impl RandomOracle {
     pub fn new(initial_prefix: &str) -> Self {
         // Since we shouldn't get such long prefixes, it's safe to assert here.
-        assert!(initial_prefix.len() < u32::MAX as usize);
+        assert!(initial_prefix.len() < u8::MAX as usize);
         Self {
             prefix: initial_prefix.into(),
         }
@@ -29,11 +29,11 @@ impl RandomOracle {
 
     pub fn evaluate<T: Serialize>(&self, obj: &T) -> [u8; 64] {
         let mut hasher = Sha3_512::default();
-        let len: u32 = self
+        let len: u8 = self
             .prefix
             .len()
             .try_into()
-            .expect("prefix length should be less than u32::MAX, tested when set");
+            .expect("prefix length should be less than u8::MAX, checked when set");
         hasher.update(len.to_be_bytes());
         hasher.update(&self.prefix);
         let serialized = bcs::to_bytes(obj).expect("serialize should never fail");
@@ -43,7 +43,7 @@ impl RandomOracle {
 
     pub fn extend(&self, extension: &str) -> Self {
         // Since we shouldn't get such long prefixes, it's safe to assert here.
-        assert!(self.prefix.len() + extension.len() + 1 < u32::MAX as usize);
+        assert!(self.prefix.len() + extension.len() + 1 < u8::MAX as usize);
         Self {
             prefix: self.prefix.clone() + "-" + extension,
         }
