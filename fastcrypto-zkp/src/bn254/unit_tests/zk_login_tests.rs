78962ef2b83a23c3deeab0b8f496a6c7c106551d
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2023-10-02 11:20:14 -0400

    chore: separate e2e workflow

--- fastcrypto-zkp/src/bn254/unit_tests/zk_login_tests.rs
+++ fastcrypto-zkp/src/bn254/unit_tests/zk_login_tests.rs
@@ -5,8 +5,7 @@ use std::str::FromStr;
 
 use crate::bn254::poseidon::PoseidonWrapper;
 use crate::bn254::utils::{
-    gen_address_seed, gen_address_seed_with_salt_hash, get_nonce, get_proof, get_salt,
-    get_zk_login_address,
+    gen_address_seed, gen_address_seed_with_salt_hash, get_nonce, get_zk_login_address,
 };
 use crate::bn254::zk_login::{
     convert_base, decode_base64_url, hash_ascii_str_to_field, hash_to_field, parse_jwks, to_field,
@@ -24,7 +23,6 @@ use ark_std::rand::SeedableRng;
 use fastcrypto::ed25519::Ed25519KeyPair;
 use fastcrypto::encoding::{Encoding, Hex};
 use fastcrypto::error::FastCryptoError;
-use fastcrypto::jwt_utils::parse_and_validate_jwt;
 use fastcrypto::traits::KeyPair;
 use im::hashmap::HashMap as ImHashMap;
 use num_bigint::BigUint;
@@ -470,63 +468,6 @@ fn test_gen_seed() {
         address_seed,
         "16657007263003735230240998439420301694514420923267872433517882233836276100450".to_string()
     );
-}
-
-#[tokio::test]
-async fn test_end_to_end_twitch() {
-    // Use a fixed Twitch token obtained with nonce hTPpgF7XAKbW37rEUS6pEVZqmoI
-    // Derived based on max_epoch = 10, kp generated from seed = [0; 32], and jwt_randomness 100681567828351849884072155819400689117.
-    let parsed_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEifQ.eyJhdWQiOiJyczFiaDA2NWk5eWE0eWR2aWZpeGw0a3NzMHVocHQiLCJleHAiOjE2OTIyODQzMzQsImlhdCI6MTY5MjI4MzQzNCwiaXNzIjoiaHR0cHM6Ly9pZC50d2l0Y2gudHYvb2F1dGgyIiwic3ViIjoiOTA0NDQ4NjkyIiwiYXpwIjoicnMxYmgwNjVpOXlhNHlkdmlmaXhsNGtzczB1aHB0Iiwibm9uY2UiOiJoVFBwZ0Y3WEFLYlczN3JFVVM2cEVWWnFtb0kiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJqb3lxdnEifQ.M54Sgs6aDu5Mprs_CgXeRbgiErC7oehj-h9oEcBqZFDADwd09zs9hbfDPqUjaNBB-_I6G7kn9e-zwPov8PUecI68kr3oyiCMWhKD-3h1FEu13MZv71B6jhIDMu1_UgI-RSrOQMRvdI8eL3qqD-KsvJuJH1Sz0w56PnB0xupUg-eSvgnMBAo6iTa0t1grX9qGy7U00i_oqn9J4jVGVVEbMhUWROJMjowWdOogJ4_VNqm67JHd_rMZ3xtjLabP6Nk1Gx-VjUbYceNADWUr5xpJveRtvb1FJvd0HSN4mab51zuSUnavCQw2OXbyoH8j6uuQAAKVhG-_Ht1hCvReycGXKw";
-    let max_epoch = 10;
-    let jwt_randomness = "100681567828351849884072155819400689117";
-
-    // Get salt based on the Twitch token.
-    let user_salt = get_salt(parsed_token).await.unwrap();
-
-    // Generate an ephermeral key pair.
-    let kp = Ed25519KeyPair::generate(&mut StdRng::from_seed([0; 32]));
-    let mut eph_pubkey = vec![0x00];
-    eph_pubkey.extend(kp.public().as_ref());
-    let kp_bigint = BigUint::from_bytes_be(&eph_pubkey).to_string();
-
-    // Get a proof from endpoint and serialize it.
-    let reader = get_proof(
-        parsed_token,
-        max_epoch,
-        jwt_randomness,
-        &kp_bigint,
-        &user_salt,
-    )
-    .await
-    .unwrap();
-    let (sub, aud) = parse_and_validate_jwt(parsed_token).unwrap();
-    // Get the address seed.
-    let address_seed = gen_address_seed(&user_salt, "sub", &sub, &aud).unwrap();
-    let zk_login_inputs = ZkLoginInputs::from_reader(reader, &address_seed).unwrap();
-    // Make a map of jwk ids to jwks just for Twitch.
-    let mut map = ImHashMap::new();
-    map.insert(
-        JwkId::new(
-            OIDCProvider::Twitch.get_config().iss,
-            "1".to_string(),
-        ),
-        JWK {
-            kty: "RSA".to_string(),
-            e: "AQAB".to_string(),
-            n: "6lq9MQ-q6hcxr7kOUp-tHlHtdcDsVLwVIw13iXUCvuDOeCi0VSuxCCUY6UmMjy53dX00ih2E4Y4UvlrmmurK0eG26b-HMNNAvCGsVXHU3RcRhVoHDaOwHwU72j7bpHn9XbP3Q3jebX6KIfNbei2MiR0Wyb8RZHE-aZhRYO8_-k9G2GycTpvc-2GBsP8VHLUKKfAs2B6sW3q3ymU6M0L-cFXkZ9fHkn9ejs-sqZPhMJxtBPBxoUIUQFTgv4VXTSv914f_YkNw-EjuwbgwXMvpyr06EyfImxHoxsZkFYB-qBYHtaMxTnFsZBr6fn8Ha2JqT1hoP7Z5r5wxDu3GQhKkHw".to_string(),
-            alg: "RS256".to_string(),
-        },
-    );
-
-    // Verify it against final vk.
-    let res = verify_zk_login(
-        &zk_login_inputs,
-        max_epoch,
-        &eph_pubkey,
-        &map,
-        &ZkLoginEnv::Prod,
-    );
-    assert!(res.is_ok());
 }
 
 #[test]
