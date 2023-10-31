9129907d0c0b091db10d38ec808964b2c9e1f80b
benr-ml <benriva@mystenlabs.com>
2023-10-31 16:03:15 +0200

    more logs

--- fastcrypto-tbls/src/nizk.rs
+++ fastcrypto-tbls/src/nizk.rs
@@ -112,7 +112,7 @@ where
 
     pub fn verify(&self, x_g: &G, random_oracle: &RandomOracle) -> Result<(), FastCryptoError> {
         let challenge = Self::fiat_shamir_challenge(x_g, &self.0, random_oracle);
-        debug!("verifying nizk with {challenge:?}");
+        debug!("verifying nizk of {x_g:?} with {challenge:?}");
         let rhs = self.0 + *x_g * challenge;
         let lhs = G::generator() * self.1;
         if lhs != rhs {
