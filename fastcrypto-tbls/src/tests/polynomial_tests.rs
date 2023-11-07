1bb49e969044b3ab93a8c9ac0112d0bb37930e2b
benr-ml <benriva@mystenlabs.com>
2023-11-07 16:11:14 +0200

    polynomials

--- fastcrypto-tbls/src/tests/polynomial_tests.rs
+++ fastcrypto-tbls/src/tests/polynomial_tests.rs
@@ -7,30 +7,58 @@
 
 use crate::polynomial::*;
 use crate::types::ShareIndex;
-use fastcrypto::groups::bls12381::G1Element;
 use fastcrypto::groups::ristretto255::{RistrettoPoint, RistrettoScalar};
 use fastcrypto::groups::*;
 use rand::prelude::*;
 use std::num::NonZeroU32;
 
-// TODO: add more tests & proptest tests.
+const I10: NonZeroU32 = unsafe { NonZeroU32::new_unchecked(10) };
+
+type G = RistrettoPoint;
+type S = RistrettoScalar;
 
 #[test]
-fn eval() {
-    let s: usize = 5;
-
-    let p = Poly::<RistrettoScalar>::rand(s as u32, &mut thread_rng());
-    let e1 = p.eval(NonZeroU32::new(10).unwrap());
-
-    let public_p: Poly<RistrettoPoint> = p.commit();
-    assert!(public_p.is_valid_share(e1.index, &e1.value));
-    let e2 = public_p.eval(NonZeroU32::new(10).unwrap());
-    let e1 = RistrettoPoint::generator() * e1.value;
-    assert_eq!(e1, e2.value);
+fn test_degree() {
+    let s: u32 = 5;
+    let p = Poly::<S>::rand(s, &mut thread_rng());
+    assert_eq!(p.degree(), s);
 }
 
 #[test]
-fn eval_regression() {
+fn test_add() {
+    let p1 = Poly::<S>::rand(3, &mut thread_rng());
+    let p2 = Poly::<S>::zero();
+    let mut res = p1.clone();
+    res.add(&p2);
+    assert_eq!(res, p1);
+
+    let p1 = Poly::<S>::zero();
+    let p2 = Poly::<S>::rand(3, &mut thread_rng());
+    let mut res = p1;
+    res.add(&p2);
+    assert_eq!(res, p2);
+
+    let p1 = Poly::<S>::rand(3, &mut thread_rng());
+    let p2 = Poly::<S>::rand(5, &mut thread_rng());
+    let mut p3 = p1.clone();
+    p3.add(&p2);
+    assert_eq!(p1.eval(I10).value + p2.eval(I10).value, p3.eval(I10).value);
+}
+
+#[test]
+fn test_eval_and_commit() {
+    // test zero
+    let p = Poly::<RistrettoScalar>::zero();
+    assert_eq!(p.eval(I10).value, RistrettoScalar::zero());
+    // test consistency
+    let p = Poly::<RistrettoScalar>::rand(5, &mut thread_rng());
+    let e1 = p.eval(I10);
+    let public_p = p.commit();
+    let e2 = public_p.eval(I10);
+    assert_eq!(e1.index, e2.index);
+    assert_eq!(RistrettoPoint::generator() * e1.value, e2.value);
+    assert!(public_p.is_valid_share(e1.index, &e1.value).is_ok());
+    // test simple poly
     let one = RistrettoScalar::generator();
     let coeff = vec![one, one, one];
     let p = Poly::<RistrettoScalar>::from(coeff);
@@ -46,50 +74,80 @@ fn eval_regression() {
 }
 
 #[test]
-fn poly_degree() {
-    let s: u32 = 5;
-    let p = Poly::<RistrettoScalar>::rand(s, &mut thread_rng());
-    assert_eq!(p.degree(), s);
-}
-
-#[test]
-fn add_zero() {
-    let p1 = Poly::<RistrettoScalar>::rand(3, &mut thread_rng());
-    let p2 = Poly::<RistrettoScalar>::zero();
-    let mut res = p1.clone();
-    res.add(&p2);
-    assert_eq!(res, p1);
-
-    let p1 = Poly::<RistrettoScalar>::zero();
-    let p2 = Poly::<RistrettoScalar>::rand(3, &mut thread_rng());
-    let mut res = p1;
-    res.add(&p2);
-    assert_eq!(res, p2);
-}
-
-#[test]
-fn interpolation_insufficient_shares() {
+fn test_recover_c0_errors() {
     let degree = 4;
     let threshold = degree + 1;
-    let poly = Poly::<RistrettoScalar>::rand(degree, &mut thread_rng());
-
+    let poly = Poly::<S>::rand(4, &mut thread_rng());
     // insufficient shares gathered
     let shares = (1..threshold)
         .map(|i| poly.eval(ShareIndex::new(i).unwrap()))
         .collect::<Vec<_>>();
-
-    Poly::<RistrettoScalar>::recover_c0(threshold, &shares).unwrap_err();
+    Poly::<S>::recover_c0(threshold, &shares).unwrap_err();
+    // duplications
+    let mut shares = (1..threshold)
+        .map(|i| poly.eval(ShareIndex::new(i).unwrap()))
+        .collect::<Vec<_>>();
+    shares.push(shares[0].clone());
+    Poly::<S>::recover_c0(threshold, &shares).unwrap_err();
 }
 
 #[test]
-fn eval_regression_msm() {
-    let one = G1Element::generator();
+fn test_recover_c0() {
+    let poly = Poly::<S>::rand(123, &mut thread_rng());
+    // insufficient shares gathered
+    let mut shares = (1..300)
+        .map(|i| poly.eval(ShareIndex::new(i).unwrap()))
+        .collect::<Vec<_>>();
+    let c0 = poly.c0();
+    for _ in 0..10 {
+        shares.shuffle(&mut thread_rng());
+        let used_shares = &shares[..124];
+        assert_eq!(c0, &Poly::<S>::recover_c0(124, used_shares).unwrap());
+    }
+}
+
+#[test]
+fn test_recover_c0_msm_errors() {
+    let degree = 4;
+    let threshold = degree + 1;
+    let poly = Poly::<S>::rand(4, &mut thread_rng());
+    let poly_g = poly.commit();
+    // insufficient shares gathered
+    let shares = (1..threshold)
+        .map(|i| poly_g.eval(ShareIndex::new(i).unwrap()))
+        .collect::<Vec<_>>();
+    Poly::<G>::recover_c0_msm(threshold, &shares).unwrap_err();
+    // duplications
+    let mut shares = (1..threshold)
+        .map(|i| poly_g.eval(ShareIndex::new(i).unwrap()))
+        .collect::<Vec<_>>();
+    shares.push(shares[0].clone());
+    Poly::<G>::recover_c0_msm(threshold, &shares).unwrap_err();
+}
+
+#[test]
+fn test_recover_c0_msm() {
+    let one = G::generator();
     let coeff = vec![one, one, one];
-    let p = Poly::<G1Element>::from(coeff);
+    let p = Poly::<G>::from(coeff);
     assert_eq!(p.degree(), 2);
     let s1 = p.eval(NonZeroU32::new(10).unwrap());
     let s2 = p.eval(NonZeroU32::new(20).unwrap());
     let s3 = p.eval(NonZeroU32::new(30).unwrap());
     let shares = vec![s1, s2, s3];
-    assert_eq!(Poly::<G1Element>::recover_c0_msm(3, &shares).unwrap(), one);
+    assert_eq!(Poly::<G>::recover_c0_msm(3, &shares).unwrap(), one);
+
+    // and random tests
+    let poly = Poly::<S>::rand(123, &mut thread_rng());
+    let poly_g = poly.commit();
+    // insufficient shares gathered
+    let mut shares = (1..200)
+        .map(|i| poly_g.eval(ShareIndex::new(i).unwrap()))
+        .collect::<Vec<_>>();
+    let c0 = poly_g.c0();
+    for _ in 0..10 {
+        shares.shuffle(&mut thread_rng());
+        let used_shares = &shares[..124];
+        assert_eq!(c0, &Poly::<G>::recover_c0_msm(124, used_shares).unwrap());
+    }
 }
