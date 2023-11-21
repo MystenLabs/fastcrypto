3006a78df20109e26efa77fe6585e0ce245c81ec
benr-ml <benr-ml@users.noreply.github.com>
2023-11-21 10:06:37 +0000

    deploy: 174aee770dcae9a6edf7059452ef6f85547d04a7

--- docs/fastcrypto_tbls/dkg/sidebar-items.js
+++ docs/fastcrypto_tbls/dkg/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Complaint","Confirmation","Message","Output","Party","ProcessedMessage","UsedProcessedMessages","VerifiedProcessedMessages"],"type":["SharesMap"]};
+window.SIDEBAR_ITEMS = {"struct":["Complaint","Confirmation","Message","Output","Party","ProcessedMessage","UsedProcessedMessages","VerifiedProcessedMessages"]};
