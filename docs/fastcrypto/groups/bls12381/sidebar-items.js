c7d7ff09ecab66b64044e5ee318a5401eda60916
benr-ml <benr-ml@users.noreply.github.com>
2024-01-08 10:51:01 +0000

    deploy: dd5adb669b895785d3f34abc562fca26c943154a

--- docs/fastcrypto/groups/bls12381/sidebar-items.js
+++ docs/fastcrypto/groups/bls12381/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["G1_ELEMENT_BYTE_LENGTH","G2_ELEMENT_BYTE_LENGTH","GT_ELEMENT_BYTE_LENGTH","SCALAR_LENGTH"],"struct":["G1Element","G2Element","GTElement","Scalar"],"type":["G1ElementAsBytes","G2ElementAsBytes"]};
+window.SIDEBAR_ITEMS = {"constant":["FP_BYTE_LENGTH","G1_ELEMENT_BYTE_LENGTH","G2_ELEMENT_BYTE_LENGTH","GT_ELEMENT_BYTE_LENGTH","SCALAR_LENGTH"],"struct":["G1Element","G2Element","GTElement","Scalar"],"type":["G1ElementAsBytes","G2ElementAsBytes"]};
