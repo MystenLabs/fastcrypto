6a8cf4e7624598114deb96a5806c469d32de4173
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2024-01-30 14:29:44 +0100

    license

--- fastcrypto-vdf/src/class_group/discriminant.rs
+++ fastcrypto-vdf/src/class_group/discriminant.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::{math::hash_prime, Parameter, ToBytes};
 use fastcrypto::error::FastCryptoError::InvalidInput;
 use fastcrypto::error::{FastCryptoError, FastCryptoResult};
