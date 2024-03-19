a1added501f25a8d1a61cf20f14e97d3282d3148
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2024-03-19 14:46:05 +0100

    Revert fixed sized arrays

--- fastcrypto-zkp/src/circom.rs
+++ fastcrypto-zkp/src/circom.rs
@@ -6,12 +6,12 @@ use fastcrypto::error::FastCryptoError;
 
 /// A G1 point in BN254 serialized as a vector of three strings which is the canonical decimal
 /// representation of the projective coordinates in Fq.
-pub type CircomG1 = [String; 3];
+pub type CircomG1 = Vec<String>;
 
 /// A G2 point in BN254 serialized as a vector of three vectors each being a vector of two strings
 /// which are the canonical decimal representation of the coefficients of the projective coordinates
 /// in Fq2.
-pub type CircomG2 = [[String; 2]; 3];
+pub type CircomG2 = Vec<Vec<String>>;
 
 /// Parse a string as a field element in BN254. Return an `FastCryptoError::InvalidInput` error if
 /// the parsing fails.
@@ -23,6 +23,10 @@ fn parse_field_element(s: &str) -> Result<Fq, FastCryptoError> {
 /// G1 point in arkworks format. Return an error if the input is not a vector of three strings or if
 /// any of the strings cannot be parsed as a field element.
 pub(crate) fn g1_affine_from_str_projective(s: &CircomG1) -> Result<G1Affine, FastCryptoError> {
+    if s.len() != 3 {
+        return Err(FastCryptoError::InvalidInput);
+    }
+
     let g1: G1Affine = G1Projective::new_unchecked(
         parse_field_element(&s[0])?,
         parse_field_element(&s[1])?,
@@ -42,6 +46,10 @@ pub(crate) fn g1_affine_from_str_projective(s: &CircomG1) -> Result<G1Affine, Fa
 /// error if the input is not a vector of the right format or if any of the strings cannot be parsed
 /// as a field element.
 pub(crate) fn g2_affine_from_str_projective(s: &CircomG2) -> Result<G2Affine, FastCryptoError> {
+    if s.len() != 3 || s[0].len() != 2 || s[1].len() != 2 || s[2].len() != 2 {
+        return Err(FastCryptoError::InvalidInput);
+    }
+
     let g2: G2Affine = G2Projective::new_unchecked(
         Fq2::new(
             parse_field_element(&s[0][0])?,
