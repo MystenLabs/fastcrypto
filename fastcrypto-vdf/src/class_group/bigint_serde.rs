d0082254f4f155f645b611d73c0d7e97db183edb
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2024-04-24 13:25:24 +0200

    test

--- fastcrypto-vdf/src/class_group/bigint_serde.rs
+++ fastcrypto-vdf/src/class_group/bigint_serde.rs
@@ -22,3 +22,29 @@ where
 {
     value.to_signed_bytes_be().serialize(serializer)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use num_bigint::BigInt;
+
+    #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+    #[serde(transparent)]
+    struct TestStruct(
+        #[serde(serialize_with = "serialize", deserialize_with = "deserialize")] BigInt,
+    );
+
+    #[test]
+    fn test_serde() {
+        let test_values = vec![
+            TestStruct(BigInt::from(-1234567890)),
+            TestStruct(BigInt::from(1234567890)),
+            TestStruct(BigInt::from(0)),
+        ];
+        for value in test_values {
+            let serialized = bcs::to_bytes(&value).unwrap();
+            let deserialized: TestStruct = bcs::from_bytes(&serialized).unwrap();
+            assert_eq!(value, deserialized);
+        }
+    }
+}
