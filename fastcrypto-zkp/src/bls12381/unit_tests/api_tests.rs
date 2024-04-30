7c1f0cb85a77ea87a292e0c78de917714331df73
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2024-04-30 10:33:30 +0200

    Add regression tests for Groth16 API (#772)
    * Add regression tests for Groth16 API
    
    * Test edge cases
    
    * fix test
    
    * clippy
    
    * fmt
    
    * Add tests to bn254
    
    * Remove debug code

--- fastcrypto-zkp/src/bls12381/unit_tests/api_tests.rs
+++ fastcrypto-zkp/src/bls12381/unit_tests/api_tests.rs
@@ -1,17 +1,23 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::bls12381::api::{prepare_pvk_bytes, verify_groth16_in_bytes};
-use crate::bls12381::verifier::PreparedVerifyingKey;
-use crate::bls12381::FieldElement;
-use crate::dummy_circuits::{DummyCircuit, Fibonacci};
-use ark_bls12_381::{Bls12_381, Fr, G1Affine};
+use std::ops::Mul;
+
+use ark_bls12_381::{Bls12_381, Fq12, Fr, G1Affine};
+use ark_ff::Zero;
 use ark_groth16::Groth16;
 use ark_serialize::CanonicalSerialize;
 use ark_snark::SNARK;
 use ark_std::rand::thread_rng;
 use ark_std::UniformRand;
-use std::ops::Mul;
+
+use fastcrypto::groups::GroupElement;
+use fastcrypto::serde_helpers::ToFromByteArray;
+
+use crate::bls12381::api::{prepare_pvk_bytes, verify_groth16_in_bytes};
+use crate::bls12381::verifier::PreparedVerifyingKey;
+use crate::bls12381::FieldElement;
+use crate::dummy_circuits::{DummyCircuit, Fibonacci};
 
 #[test]
 fn test_verify_groth16_in_bytes_api() {
@@ -187,6 +193,91 @@ fn test_verify_groth16_in_bytes_multiple_inputs() {
         &pvk[2],
         &pvk[3],
         &inputs_bytes,
+        &proof_bytes
+    )
+    .unwrap());
+}
+
+#[test]
+fn api_regression_tests() {
+    // Prepare VK
+    let vk_bytes = hex::decode("835da56c560fbba42fe472c9c6c687986953de12db2adb66c10ecfff8957c1ec28a030dd2512b1ef3afa09fff2b467ddad48984a12c6568511bca1a3662ecfba801f31422f5c4208986bc52186938a2d86745abc9e6e0503b5eb16c5f2a622e108013d74b85f9532d87c391c5b0c49557ce47221e869b329fbe103ca73136c70b708ac61fa0092a238bf9060dc885d0da59dae8e121fd0013e45116a3d63837949b7976fd15e99d9974c654638d5fa1cbd51531a00889c75aeb5a91dd54891fd0b6c37bc2d6817541e4818040b3e2c78451674c5f180a895107070592eabf3386d21a9f6a91adb1f94debadb604f47c6a412f66347289eb47eec60604b31d973697f7f4ffc792fa914e24286f6d277a002ac1275b83a8dad2d84fcc4fc771bbf0854b5e655be18c6089cfc1841cf311e3c43f1ee2cd371d42cc91d61b5b2848f448f9f57229781d6dba2ca8367f2e3f602000000000000009274be44d5c4ec5203e96cb2b3e96468062cf4eebea465b1721889924bbea43d26de7c0b312beb7c09182c278f54c4d2b4092ebefd19ba81e8d5d787b25cd8ca8d03130326032c0e2f01d23cdf4e7a845f2c00a38574182e08d2f9fbe90ca3f7").unwrap();
+    let pvk_expected = vec![
+        hex::decode("9274be44d5c4ec5203e96cb2b3e96468062cf4eebea465b1721889924bbea43d26de7c0b312beb7c09182c278f54c4d2b4092ebefd19ba81e8d5d787b25cd8ca8d03130326032c0e2f01d23cdf4e7a845f2c00a38574182e08d2f9fbe90ca3f7").unwrap(),
+        hex::decode("5e235118ea90b0347c840b5f29df0cb765a81e9d55a0879fad4fda927f559aaa78af3f0d0ac1a0a3debe86e08c608907622cf46ce3be71d559c6bc460a206db1ac7ec28ef8817efd75a4ca6b56243ce2bd00782fd84ee9238c27b64bf68b4a138b22e9829464497fde48632b0a94461c6fc7dffab47d23851e30a484fb823827620b14678cb80ba4125a305b99213619354a7f98923b154ae209440b0e631ec0da2b02946dad1ad20830fe32f92e512843d6f870b0b4fa290e62935f921d470b0b8058af1f51391ebaf4ab1869933cfe37725e654a00f2c4a7099863937f4ce23e01bf7b2cabea888c6b073f47a0f411a3dc9295164e46a665c71c4ee31dd88f4c4d25aee40c832fc79bbb9f0bcbcf3fb3a5868a7027320f719f9ce0d23c620989e30af71459b32abffd7e879adfdc6d3417d552f43fbfa8b295bfd661dfc9fe2ac846c28f8527506ac15b1247dc7800d040e25698d2797be8c357a6883e0eeca928451406c93e0e3a2ea5fecbc5165e6691d164b6fa4de207a338a882600802a835795b29ee7ae73c66d6d22104a77e010d05293fc3fed65772b0da969a63c384ae24720e9c24c373912446400e7b087ecb1cbfe772b574bcf7c2e1e61fe56d9abe57ff5298956aa4ba47dd77c703f0fc4c69ab61d6e6e4161243e490a85b0c9d1dde1764f4924a3fc27de397256eb4822be1ba1faa6840afaa3219570806fe78622dc5ac74cb7352a0e4fa0fff5e0d30b6f87fd1a3e5fd3256f807ea5b5a45911518f4b93ec96f88a771ca33dae1339fcc7047cd0740b5df25e4403051af0b").unwrap(),
+        hex::decode("859dae8e121fd0013e45116a3d63837949b7976fd15e99d9974c654638d5fa1cbd51531a00889c75aeb5a91dd54891fd0b6c37bc2d6817541e4818040b3e2c78451674c5f180a895107070592eabf3386d21a9f6a91adb1f94debadb604f47c6").unwrap(),
+        hex::decode("8412f66347289eb47eec60604b31d973697f7f4ffc792fa914e24286f6d277a002ac1275b83a8dad2d84fcc4fc771bbf0854b5e655be18c6089cfc1841cf311e3c43f1ee2cd371d42cc91d61b5b2848f448f9f57229781d6dba2ca8367f2e3f6").unwrap(),
+    ];
+    let pvk_actual = prepare_pvk_bytes(&vk_bytes).unwrap();
+    assert_eq!(pvk_expected, pvk_actual);
+
+    // Verify
+    let vk_gamma_abc_g1_bytes = hex::decode("896f65ec302f346365c3ebdf8812aa98dc003bd93c663f72d8f9cbb9f2b73b3a1ba0bad12678af22378ac625d414dda8a44e2ed104340b4d9e03fba0e2a3d27efcdfa41fcec60b1071c17089247d6b690bb18fccaccf8a3c0bd8416a160c7c8f8fcdf3f68ae2c9c84d4bc13ef6a64fb865ae36f077f45ee37fa4ebd43da333420a6102befa7fc2fc0c7e85d3722db89a").unwrap();
+    let alpha_g1_beta_g2_bytes = hex::decode("ce35f0234d308739dabe6687ea37d794a6542c802fde201625c6d4c55daf9a15fbdc6628b1a8d6db7fb930c58252750e61af062aeba4ea368bc95999f87dbd93995cfc0bd9c539844c3633b6f58951a9988bf1d96b49ef0aca6c98c84a16850557afaebb7e9c2b926ee436a96a3611161605d438b86a1d9933d87afbdaa9cace569b810ded82ec6f23801c3df1bd8203513fec813d706f23e5afe0187fa552d6ea8547e1b82740858e9ef4139ad9a952303a63395461dc7d78e05688afe2c50860ca60a5564f00344cbecdc4ce94ac9fb41106642d7c3d6445d6607b9b963cd05975f4ba9b4674159b5b7458092fce0f532f1a8dfd76aca31d3978a651e020dcd3577674cba022733afb97d96b91f8cc5590ea4147d6130fb24895210b5e2d02d37dcf6b30245ef98543086859016b1f38d56f79100fc608339367b9bf9608e8175e4e602c853281805cb110a1ea2a0ef8c7aa220a49224b2a7de996996d2350dce3905e6cd1e6ab850f3ccccefc1c0b84baa13c9054b32d0d42f869d17f6c06e6831e11b2e65d07fad4b25eb540630505d23635ef1effb35626042c394190d5c5b9062f296546cd5517c9a8352a4310b5469e4321cc1d86db15d6475c1d0652f8e57c220cfe8eb798b3595a516c51bcea81efb0d8dace2521bae3537d721919e8dbb45fbf2ecf47e7483c7d725dbb47ec191c5bf6de4f7487b23fc1c04c6ba7ce2001d64f6236d025b88aaadecdde1482812836fc6f4d6efcbeae1ba2836b5c57399c28b0f58649bf6c7a743bb2e1469d348e1bcfc75ff18976d825d72ab716").unwrap();
+    let gamma_g2_neg_pc_bytes = hex::decode("89a91d003a78b3580981407bfd5c1f70b329f7b7479fbbd244be8729def2a3538901ff7be9320cf6f9773e7ba4c2712a0b9061adfd335bf4e378414fe788766e8feb5cdf2a0e5368c8451a40ce1b1c54e5c0e997a37e1e97b228ac222ae49b95").unwrap();
+    let delta_g2_neg_pc_bytes = hex::decode("8b0624cf881e828bd01eabe53992962f3bd9e444375900ed2a875a74c705597f80d5c77c5f698346082ca0dda33344f7059702c4ea9044f417e44575337b6d2f85202160a00c30d62a02ad84abc0dcbda1a834fb73021a1c500b0f021e41f868").unwrap();
+    let public_inputs_bytes = hex::decode("7b00000000000000000000000000000000000000000000000000000000000000c801000000000000000000000000000000000000000000000000000000000000").unwrap();
+    let proof_bytes = hex::decode("826a9c4a653a73bda4782c5ea566e1eb556cd516a820566ec3237f2bf50432777a11c32480ad09d9f7395785cbd4a338a53c93a906cb947d94a07524a57c852cc9daff85d50e6597327a12dd6b588dd39771e87e37ac276d59125714a15d0af204bff8ede3d42b85b8c76b842ae88209113afc886d8f9875ade9293c9059ecfe8a5483b023eb845f20cd0f422e8acdbba51e23ccf4feeaf82a05efd7b803cc9d7244cb53c119a1e4695b945e2663bfc90c639a6ec39237a01a24958585438ab3").unwrap();
+    assert!(verify_groth16_in_bytes(
+        &vk_gamma_abc_g1_bytes,
+        &alpha_g1_beta_g2_bytes,
+        &gamma_g2_neg_pc_bytes,
+        &delta_g2_neg_pc_bytes,
+        &public_inputs_bytes,
+        &proof_bytes
+    )
+    .unwrap());
+
+    // Zeros as public inputs
+    let vk_gamma_abc_g1_bytes = hex::decode("8e954e274e57e8f5b7e35f26a5656e13bc8aecdc21356cdae06113990f0aa7661f842c274823ee55b064f3be7d7dbd16ae9ad36e2aef40c9b9046145dd4f969d3933d41caca8f8e035b31fbe152a66aa73c6882f9facbf4600b6b86967b22b5691f6d5353d418b4d14b724a66959ea1693f04bbe2272b250286632ef7f808833e74b5987bcd5ae2255c16807d031185a").unwrap();
+    let alpha_g1_beta_g2_bytes = hex::decode("2c2c4d1819ec799bf861c9b1901f275a4c2df57e009a2d90d201d304fcd4945e07d065a02e447cadf7738220d299460dfa5053ff66e20e4ce1451887f3c4e4be7bf4ec95461c84efe461ce0123711d5cae16594197325cb69d786e83c995a611cf885dde4ed4c30db200bf6bc064fa81b0d5ecb65ac817be597aa117d930688ec881e8dc7c88b518137f7c94bfc8d0189956357496d8c44ae433a8dd688c2a63ea080df898e0e035c2673b342c911d5f27b411b33e99f038f4bb64e40384580f2c4ce255ed949399c38576be9d052337e7111c2457c7471f4d36e51ed979ac48ff4aee14d0368342be9756be1482f515bbfb2b7d2089d1654f751af828a5d94aeb1f22e0d02c40734324dddab49aca522411a6c11c0d6633d9f92b4fdc964e139e5b230be6f9e31a5ddd5995fe66f3eb3c4b1ed052c868a5de1466baa1ae23b132dccc90120460cbb8209754bdecbf00c7ac892c017ce784f13e1cfcb6438f0402dbadf4f61abde6decdfebcaff344fa6a2e43a0126df3c6b3d0d60c96e484007260d5d03e1aa819046c0888fae6351ae962f54f0fd699dfcf8501aec23d5ee90061d98567372307f25cc96a2f27550a233800ae30ce5fc94e2e1b815b38739b26206a5e8d4a1d30d5beb8ed78d3c5c67e1e32a38d59ef610c16de2de920a40b6f0068bb878516c899a47bfdc686c9d82acc91420ecca2fe11bb978176e5f1215f88ca073654f2f2c94d3a6a44810711d7ecf60cc07b0fc16a84aae378e36930c39b98f2eb5b45d086b45412cc638fc32b65cc5aadfe8e9f73c6bea7a7921317").unwrap();
+    let gamma_g2_neg_pc_bytes = hex::decode("ae02f7ff1f3220149bd88aa0dcc9f970f98ca6410dca0ef89f1f36f62c24b3f19f7bb0307c537461fa66320602cf623308d43485e95f07a4fac65516ea119253c6e676ca5a15af4275d2b2a965a75cde6dc83a9a6b2e3a8fe64f7004fc689279").unwrap();
+    let delta_g2_neg_pc_bytes = hex::decode("a1f01a5a7957b6dbc7c893ef092ce7a5064295088d8c20568dabcfcd3caf6bea997c7584001b2cb1a8072b57520e23501758ec3c8f3783dfedf015cac4c3355bf74f0425e253d8132dbb91d58c2b6e83d1b7a7cc2141240c72f8576cb282823d").unwrap();
+    let public_inputs_bytes = hex::decode("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+    let proof_bytes = hex::decode("ab8168e3201c98ecc444b84708af95cc5bfc6a6a3db3501fe70e5257b49c349f958c6b84985744baf0c063cad559132f84eb8533ef4ff0d05a9abeefe41f62c17cdc8f5166f9bc5c89b6b8590f26f04e586a99412ce3f6363934e30acfb12b290c1800d19c7c83b4295afb85562a69a0dec80537c5d12ecf532d8e3a7a583f3cc44bd4ad7311704bdfcf030a050579f9b4a8bbe30e87e571feb3e2fd8e40aad886ebf1ae5f3a9dbbee168e9e380d2a2d2c6aadbd5dc44116f7bb01a1277f2ed0").unwrap();
+    assert!(verify_groth16_in_bytes(
+        &vk_gamma_abc_g1_bytes,
+        &alpha_g1_beta_g2_bytes,
+        &gamma_g2_neg_pc_bytes,
+        &delta_g2_neg_pc_bytes,
+        &public_inputs_bytes,
+        &proof_bytes
+    )
+    .unwrap());
+
+    // Trivial proof with all zeros/identity elements
+    let mut vk_gamma_abc_g1_bytes = fastcrypto::groups::bls12381::G1Element::zero()
+        .to_byte_array()
+        .to_vec();
+
+    // Expects two public inputs, so the vk needs three elements here
+    vk_gamma_abc_g1_bytes
+        .extend_from_slice(&fastcrypto::groups::bls12381::G1Element::zero().to_byte_array());
+    vk_gamma_abc_g1_bytes
+        .extend_from_slice(&fastcrypto::groups::bls12381::G1Element::zero().to_byte_array());
+
+    // The API expects serialization like in Arkworks for GT elements
+    let mut alpha_g1_beta_g2_bytes = vec![];
+    Fq12::zero()
+        .serialize_compressed(&mut alpha_g1_beta_g2_bytes)
+        .unwrap();
+
+    let gamma_g2_neg_pc_bytes = fastcrypto::groups::bls12381::G2Element::zero().to_byte_array();
+    let delta_g2_neg_pc_bytes = fastcrypto::groups::bls12381::G2Element::zero().to_byte_array();
+    let public_inputs_bytes = hex::decode("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+
+    let mut proof_bytes = vec![];
+    proof_bytes.extend_from_slice(&fastcrypto::groups::bls12381::G1Element::zero().to_byte_array());
+    proof_bytes.extend_from_slice(&fastcrypto::groups::bls12381::G2Element::zero().to_byte_array());
+    proof_bytes.extend_from_slice(&fastcrypto::groups::bls12381::G1Element::zero().to_byte_array());
+
+    // The trivial proof should pass verification
+    assert!(verify_groth16_in_bytes(
+        &vk_gamma_abc_g1_bytes,
+        &alpha_g1_beta_g2_bytes,
+        &gamma_g2_neg_pc_bytes,
+        &delta_g2_neg_pc_bytes,
+        &public_inputs_bytes,
         &proof_bytes
     )
     .unwrap());
--- fastcrypto-zkp/src/bn254/unit_tests/api_tests.rs
+++ fastcrypto-zkp/src/bn254/unit_tests/api_tests.rs
@@ -5,7 +5,8 @@ use crate::bn254::api::{prepare_pvk_bytes, verify_groth16_in_bytes};
 use crate::bn254::verifier::PreparedVerifyingKey;
 use crate::bn254::VerifyingKey;
 use crate::dummy_circuits::{DummyCircuit, Fibonacci};
-use ark_bn254::{Bn254, Fr};
+use ark_bn254::{Bn254, Fq12, Fr, G1Projective, G2Projective};
+use ark_ff::{One, Zero};
 use ark_groth16::Groth16;
 use ark_serialize::CanonicalSerialize;
 use ark_snark::SNARK;
@@ -556,4 +557,93 @@ fn fail_verify_groth16_invalid_elusiv_proof_in_bytes_api() {
         &elusiv_sample_proof_invalid_pin.0
     )
     .is_err());
+}
+
+#[test]
+fn api_regression_tests() {
+    // Prepare VK
+    let vk_bytes = hex::decode("3c747dd28b1d21b2be3dae04a8c88152d2a5cec5efc800f7a4feea5f938e248298b4d9f57babfa38799f78172df2c303a754945bba9235b39be37826e4c15b273a0f051b814c50a489f6845bfe80521a1e99fbce0c6fda1ba521d4c29ea8d29e3cb543973711905d0cda7dee3c71e7b9460c34e9e7285e03d5fa519e2986a428754b79e5ddb20de39d9afb7b901b4a60c2366c20e658b476206a1710f99146914633127ccae0e459b3d2a8aa39db9a6d5aa91b5f7038bc2bbd7e9e713e6a9b2fb254de7356d1b449f95168ed30e5570715367c99b0e07fd8595517cdb042432b0200000000000000ada96147db3a3bef28beecff2d9bf2814bf07a62722e69486b6e2c3cd26844ac0779d941feabcdc6ae03c87f33fc46877f0261428e29bbf1087e8f858e1dfa88").unwrap();
+    let pvk_expected = vec![
+        hex::decode("ada96147db3a3bef28beecff2d9bf2814bf07a62722e69486b6e2c3cd26844ac0779d941feabcdc6ae03c87f33fc46877f0261428e29bbf1087e8f858e1dfa88").unwrap(),
+        hex::decode("4d6005b84ae8c96c5ecce7218b59b20a8a6b0ae7b0b4f3d3aa42407f161ddf140b4b211677e840d34950a4a780f91a434304870569e2ab9ee2fe4e3b4b712e2a2e2977ee78da6db29c0c012a13442df99620b008e38a9ea65dbb19a00e10412fc8558f6687fc2f8420ba8546e86dd91779897998a4bbd6d38c3cfd1af46f4d28f5bf00faf918a2c1079b216d03036920a89427bf00d01d17da30adbb9710952785b2dbd6a5e4ae828a970a5862c1c04fc082cf2ff694cbeefb92317dfabc3b21f1e3f17e25aaecbeaabfcd76b72fc9ba6494330ce687ed522bb22468e289dd103212f554cefc72ca9f49717f8aeefe807b4c44260391db2088be32142061921adf9efc013ce13a83aaa856a39480356581c8d2369a8c2b80c418607ec5778804703da5bbff5faf2aab898edd35b694ec5cbd6c8924d5ee5dded531875bcd462636133f8dcb4b858f34c02da0a6a4a12283fbe6def67b54f29f515533da05860430048966aeaa077a536cdc20972c2243de0c15febedcd3241d32c50df205f82a").unwrap(),
+        hex::decode("3cb543973711905d0cda7dee3c71e7b9460c34e9e7285e03d5fa519e2986a428754b79e5ddb20de39d9afb7b901b4a60c2366c20e658b476206a1710f9914611").unwrap(),
+        hex::decode("4633127ccae0e459b3d2a8aa39db9a6d5aa91b5f7038bc2bbd7e9e713e6a9b2fb254de7356d1b449f95168ed30e5570715367c99b0e07fd8595517cdb04243ab").unwrap(),
+    ];
+    let pvk_actual = prepare_pvk_bytes(&vk_bytes).unwrap();
+    assert_eq!(pvk_expected, pvk_actual);
+
+    // Verify
+    let vk_gamma_abc_g1_bytes = hex::decode("df35140d037b211901de04f62a417c53c304328304d738bcc4ff623b1e65698765c6fa5f804bac35ce67f2c26e37bd0cba6f8a944a6c8c17a2417c7b88254f000f87f4f3b509521fbd63e8460ef9082880ada8f342803fd2188e5ce75bbb0d06").unwrap();
+    let alpha_g1_beta_g2_bytes = hex::decode("b51f110c1840d39a3185436f5307ad47b375ff892acd78cfa2939556eb46d22112f76ffdd8f3be5fabcc44df3a6750edaaf65af3218f58988afedf6b4202b41066a18f1a8e6b06194838ab7331524da9389802cfd1cc87ef800cbf927fe65e1009fd4d603bf4790371ea2272d17f9a1d98f790b04af2fef1f35889b7e54cc210ce1e47a6ecece6f9d20e8c4514db8f74b96db01cd3528e49faf67d40b7e18f12cd4aadf7698d3fbca9d5a660f677a8c9bba800d3a125c9431e151fa9120c9d07c80d5f662fd6630f1c7441ce7c2f65cad89898222976d519fcadcd9a9beb7120625ee32258e4436e31b4661521c68dbd78027a0935423f2f3b74087c3dcc4b15f8c23a499e1b2e0e456d24274eb7e5c2a0ca3338c5e4ec4d31fd3392c32c790e05142edd5fe2e671260f33318826f2ce0e0cbadb2d347a1644d14824a1f2f402c2740188daef879da5139d06160b1bd44788a272e92d33553631cbc9db991f18606ba945064aff9ec7c0d6d0fde961d834783008718783f67c778dd952b1270f").unwrap();
+    let gamma_g2_neg_pc_bytes = hex::decode("04d7dbe72328896134ed56890042202827ce6805cd384ab00afa0997b26a8d0905b8d48c57bcbc7e9d8a2ff6771a50f90d96970636a9e7d89aaf7369ce7e4d9f").unwrap();
+    let delta_g2_neg_pc_bytes = hex::decode("06639a71c443532e6c7797e0b18926ea09962bd2fdad9a7130df397463afd315b4bfda26cbef66c4f62f6e65a90d70f058df1f57216846e93890f9b2be68ce85").unwrap();
+    let public_inputs_bytes = hex::decode("7b00000000000000000000000000000000000000000000000000000000000000c801000000000000000000000000000000000000000000000000000000000000").unwrap();
+    let proof_bytes = hex::decode("537a1b8ba0fc0f2b456c59769a58f22fbf9228311f5cc9070e7358e28c51d2033984c58d47b747c7e07bc87520f14e19029042259a21a910040b87533bf3c32613261dbeffd2913f3627895532b49e9cf623fdfa94e4ef9c040086b397ad640c54c165811c46fa50a880a7b8616d2165848b8f631db25b1b1ebf7fe021f4a11a").unwrap();
+    assert!(verify_groth16_in_bytes(
+        &vk_gamma_abc_g1_bytes,
+        &alpha_g1_beta_g2_bytes,
+        &gamma_g2_neg_pc_bytes,
+        &delta_g2_neg_pc_bytes,
+        &public_inputs_bytes,
+        &proof_bytes
+    )
+    .unwrap());
+
+    // Zeros as public inputs
+    let vk_gamma_abc_g1_bytes = hex::decode("7329e3fb0f59e62c7a88ec42ca108a94f525298de1fa7640123daa7469fd1102dacd7b18f373a7ecf75fba98b812367196ff4e62ad4653f0feba143a6ec268a9442f7562a719360f83480bf4624adfdb80faaf2302b0a6022caf32de5a120d8d").unwrap();
+    let alpha_g1_beta_g2_bytes = hex::decode("b45caf631bc5d75cf3f00a7520380275d826a41b5641e1acba32b29aea45661480ed201e35888a845744ee5565688aedcc4a25f50fedfc169479285476fc7100a1f525423181d8a1088ee24a9b7e8f91d0bc3465493b591172f3a1a8330f911a2681fee88569d6eac0be6e741f2edd22a244f937db29524a3ed9ddd79112202d194bdc21fe7b374fee3cd4b6395ecb14986054a2b3e2348c496ed12f5db1c806dd1df76a9faa0f62118ea4eb217735206196cf16284f7b5d1bb7fdabc323f02022550841d98b365c22d5d962f4c544bab40059dc5cf576a3c31a1e00cd107c2549df1e99e8837b2e8fe9e846a0f8fdbdf0d47f1051bc280d6cf41fa205d1b72a5be7f6dacca14fc38f83adfa72a38c870cb1e2d638bba767677f087ab65bdd12c9f857334cbcb8d4cdffb901c9ce6123d2525bd93765db4214900ba26a26ce06fa5c661f0a38affe716a95876b394cc20b6862dd112e01be3dec14d7f76b441049cbd5ca74358fd4f4c4027f28b7028a4f49f20129eb28b0f0e861e34bf80805").unwrap();
+    let gamma_g2_neg_pc_bytes = hex::decode("f43620ffdf673339d95fbd4b40fbce43b7d4487b58b13f8c9ab146de9c992910ee2953bbad19f7374146404f9fb9a4d4c4668d999e1a125357dcfe8d8d6aaa19").unwrap();
+    let delta_g2_neg_pc_bytes = hex::decode("fbb7656bf9f28ccfbeb13b78811bd4fb8cc4dcc301bccf8b7f604c60a13caf1996a518acb5ee768617e54dfab6aa3743881a62b2dfd4222fa13d4f85de913216").unwrap();
+    let public_inputs_bytes = hex::decode("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+    let proof_bytes = hex::decode("52ce4e209a74e81322849ec341378c332cdb08ba74cd8804e763d4d1af73778cce951ab584d2100a1fd9edd2eec8e8f929029bdd9cf8f9fdaccb683368e797092f5d1415f6e26a8604dd87a85b66faa68fcb00627952d21dbeb8392e52692c83f0fc821e2bb35e09cfb3316d9266a2de90d79d67344b6d727b6eac573a0a6d8a").unwrap();
+    assert!(verify_groth16_in_bytes(
+        &vk_gamma_abc_g1_bytes,
+        &alpha_g1_beta_g2_bytes,
+        &gamma_g2_neg_pc_bytes,
+        &delta_g2_neg_pc_bytes,
+        &public_inputs_bytes,
+        &proof_bytes
+    )
+    .unwrap());
+
+    // Trivial proof with all zeros/identity elements
+    let mut g1_inf_bytes = vec![];
+    G1Projective::zero()
+        .serialize_compressed(&mut g1_inf_bytes)
+        .unwrap();
+
+    let mut vk_gamma_abc_g1_bytes = vec![];
+    vk_gamma_abc_g1_bytes.extend_from_slice(&g1_inf_bytes);
+    vk_gamma_abc_g1_bytes.extend_from_slice(&g1_inf_bytes);
+    vk_gamma_abc_g1_bytes.extend_from_slice(&g1_inf_bytes);
+
+    let mut alpha_g1_beta_g2_bytes = vec![];
+    Fq12::one()
+        .serialize_compressed(&mut alpha_g1_beta_g2_bytes)
+        .unwrap();
+
+    let mut g2_inf_bytes = vec![];
+    G2Projective::zero()
+        .serialize_compressed(&mut g2_inf_bytes)
+        .unwrap();
+
+    let gamma_g2_neg_pc_bytes = g2_inf_bytes.clone();
+    let delta_g2_neg_pc_bytes = g2_inf_bytes.clone();
+    let public_inputs_bytes = hex::decode("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+
+    let mut proof_bytes = vec![];
+    proof_bytes.extend_from_slice(&g1_inf_bytes);
+    proof_bytes.extend_from_slice(&g2_inf_bytes);
+    proof_bytes.extend_from_slice(&g1_inf_bytes);
+
+    // The trivial proof should pass verification
+    assert!(verify_groth16_in_bytes(
+        &vk_gamma_abc_g1_bytes,
+        &alpha_g1_beta_g2_bytes,
+        &gamma_g2_neg_pc_bytes,
+        &delta_g2_neg_pc_bytes,
+        &public_inputs_bytes,
+        &proof_bytes
+    )
+    .unwrap());
 }
