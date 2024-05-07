f50fbe00bba4c9387d788e5a7d35703390c4c40a
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2024-05-07 19:45:12 +0200

    Update test

--- fastcrypto-vdf/src/vdf/wesolowski/mod.rs
+++ fastcrypto-vdf/src/vdf/wesolowski/mod.rs
@@ -129,7 +129,8 @@ mod tests {
 
         // Regression tests
         assert_eq!(bcs::to_bytes(&output).unwrap(), hex::decode("2024ff799e22587cc344b83ace4ce2d814d52aba7dd711591fd407e31a55822a1e20051fa84e09b4bd5a6ee02b6fae24db625863ae31cfaf962f2f490d92dd372e3f2101714cc516048bd26c8788ead8318dcaa0c3fe615ece8164cf56a4bdea90c8fc6f").unwrap());
-        assert_eq!(bcs::to_bytes(&proof).unwrap(), hex::decode("206073d81fed637a8640632a41d040c22851cd240eabb1346994bd996e8f018b7620b2a15fe0116fc1dfc6e33088cbd1615423b92fee08ec2077f794df1886436c6721009d1b3148590a3e1097ab83ad6bcceec1fcee78da679bddae9213dbd930b8684d").unwrap());
+        println!("{:?}", hex::encode(&bcs::to_bytes(&proof).unwrap()));
+        assert_eq!(bcs::to_bytes(&proof).unwrap(), hex::decode("2051242499ff4467ba4d9bd858d6609b3a29736a80e09aa3edc6fa4ca49292daae20c066a1b52a8c5a0ff230224fc403dec11ee156e523f22b78ab8abc9706ae38c52100b4c56b5d7815425677bbe0ddc924d6277a237e4c5317b10a1a36039148d1d19c").unwrap());
 
         assert!(vdf.verify(&input, &output, &proof).is_ok());
 
