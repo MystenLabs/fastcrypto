519a6bf66f838a811e9254b34632934dbf9beb5a
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2024-08-22 21:12:27 +0200

    simplify

--- fastcrypto-zkp/src/bn254/poseidon/mod.rs
+++ fastcrypto-zkp/src/bn254/poseidon/mod.rs
@@ -83,7 +83,7 @@ pub fn poseidon_merkle_tree(inputs: &[FieldElement]) -> FastCryptoResult<FieldEl
         poseidon_merkle_tree(
             &inputs
                 .chunks(MERKLE_TREE_DEGREE)
-                .map(poseidon_merkle_tree)
+                .map(poseidon)
                 .collect::<FastCryptoResult<Vec<_>>>()?,
         )
     }
