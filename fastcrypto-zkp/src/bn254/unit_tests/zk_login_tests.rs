ed2f792fd8368de88cbc4e5a818bf3710d62313c
vlegakis <vassilis.legakis@mystenlabs.com>
2024-09-28 12:47:52 +0300

    Update test cases

--- fastcrypto-zkp/src/bn254/unit_tests/zk_login_tests.rs
+++ fastcrypto-zkp/src/bn254/unit_tests/zk_login_tests.rs
@@ -441,6 +441,7 @@ async fn test_get_jwks() {
         OIDCProvider::Microsoft,
         OIDCProvider::AwsTenant(("us-east-1".to_string(), "us-east-1_LPSLCkC3A".to_string())),
         OIDCProvider::AwsTenant(("us-east-1".to_string(), "us-east-1_qPsZxYqd8".to_string())),
+        OIDCProvider::AwsTenant(("eu-west-3".to_string(), "eu-west-3_gGVCx53Es".to_string())), //Trace
         OIDCProvider::KarrierOne,
         OIDCProvider::Credenza3,
         OIDCProvider::Playtron,
@@ -448,7 +449,7 @@ async fn test_get_jwks() {
         OIDCProvider::Onefc,
         OIDCProvider::FanTV,
         OIDCProvider::Arden,
-        OIDCProvider::Trace,
+        OIDCProvider::Arden,
     ] {
         let res = fetch_jwks(&p, &client).await;
         assert!(res.is_ok());
