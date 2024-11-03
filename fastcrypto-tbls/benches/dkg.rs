222852e1f4281deb848d36ae113127ad10e5dae1
benr-ml <benriva@mystenlabs.com>
2024-11-03 13:54:34 +0200

    one more

--- fastcrypto-tbls/benches/dkg.rs
+++ fastcrypto-tbls/benches/dkg.rs
@@ -4,6 +4,7 @@
 use criterion::{criterion_group, criterion_main, BenchmarkGroup, Criterion};
 use fastcrypto::groups::{bls12381, ristretto255};
 use fastcrypto_tbls::dkg_v1::Party;
+use fastcrypto_tbls::ecies_v1;
 use fastcrypto_tbls::nodes::{Node, Nodes, PartyId};
 use fastcrypto_tbls::random_oracle::RandomOracle;
 use itertools::iproduct;
