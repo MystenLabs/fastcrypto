bdeaf411537435624f40125b05c75ba895433911
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2025-01-02 11:11:10 -0500

    add experimental flag

--- fastcrypto-tbls/benches/nidkg.rs
+++ fastcrypto-tbls/benches/nidkg.rs
@@ -13,7 +13,6 @@ type G = bls12381::G1Element;
 
 fn gen_ecies_keys(n: u16) -> Vec<(u16, ecies_v0::PrivateKey<G>, ecies_v1::PublicKey<G>)> {
     (0..n)
-        .into_iter()
         .map(|id| {
             let sk = ecies_v1::PrivateKey::<G>::new(&mut thread_rng());
             let pk = ecies_v1::PublicKey::<G>::from_private_key(&sk);
@@ -54,7 +53,7 @@ mod nidkg_benches {
         {
             let mut create: BenchmarkGroup<_> = c.benchmark_group("NI-DKG create");
             for n in SIZES {
-                let t = (n / 2) as u16;
+                let t = n / 2;
                 let keys = gen_ecies_keys(n);
                 let d0 = setup_party(0, t, &keys);
 
@@ -67,11 +66,11 @@ mod nidkg_benches {
         {
             let mut verify: BenchmarkGroup<_> = c.benchmark_group("NI-DKG message verification");
             for n in SIZES {
-                let t = (n / 2) as u16;
+                let t = n / 2;
                 let keys = gen_ecies_keys(n);
                 let d0 = setup_party(0, t, &keys);
                 let d1 = setup_party(1, t, &keys);
-                let m = d0.create_message(&mut thread_rng()).unwrap();
+                let m = d0.create_message(&mut thread_rng());
                 println!("Message size: {}", bcs::to_bytes(&m).unwrap().len());
 
                 verify.bench_function(format!("n={}, t={}", n, t).as_str(), |b| {
@@ -84,11 +83,11 @@ mod nidkg_benches {
             let mut verify: BenchmarkGroup<_> =
                 c.benchmark_group("NI-DKG message processing for one share");
             for n in SIZES {
-                let t = (n / 2) as u16;
+                let t = n / 2;
                 let keys = gen_ecies_keys(n);
                 let d0 = setup_party(0, t, &keys);
                 let d1 = setup_party(1, t, &keys);
-                let m = d0.create_message(&mut thread_rng()).unwrap();
+                let m = d0.create_message(&mut thread_rng());
 
                 verify.bench_function(format!("n={}, t={}", n, t).as_str(), |b| {
                     b.iter(|| d1.process_message(&m, &mut thread_rng()))
@@ -101,7 +100,7 @@ mod nidkg_benches {
             let mut verify: BenchmarkGroup<_> =
                 c.benchmark_group("NI-DKG generate partial pks in g2");
             for n in SIZES {
-                let t = (n / 2) as u16;
+                let t = n / 2;
                 let keys = gen_ecies_keys(n);
                 let d1 = setup_party(1, t, &keys);
 
@@ -114,7 +113,7 @@ mod nidkg_benches {
             let mut verify: BenchmarkGroup<_> =
                 c.benchmark_group("NI-DKG verify partial pks in g2");
             for n in SIZES {
-                let t = (n / 2) as u16;
+                let t = n / 2;
                 let keys = gen_ecies_keys(n);
                 let d0 = setup_party(0, t, &keys);
                 let pks_in_g2 = d0.create_partial_pks_in_g2();
@@ -122,7 +121,7 @@ mod nidkg_benches {
                     "pks_in_g2 size: {}",
                     bcs::to_bytes(&pks_in_g2).unwrap().len()
                 );
-                let m = d0.create_message(&mut thread_rng()).unwrap();
+                let m = d0.create_message(&mut thread_rng());
 
                 verify.bench_function(format!("n={}, t={}", n, t).as_str(), |b| {
                     b.iter(|| {
