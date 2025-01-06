d17fb5396a01d1dad2333044ebde260f67b28c4a
joyqvq <joyqvq@users.noreply.github.com>
2025-01-06 18:49:23 +0000

    deploy: 6ce2022adbe13abd68e1efc33705b2c782e6e4db

--- docs/trait.impl/alloc/string/trait.ToString.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["fastcrypto_zkp",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"fastcrypto_zkp/bn254/zk_login/enum.OIDCProvider.html\" title=\"enum fastcrypto_zkp::bn254::zk_login::OIDCProvider\">OIDCProvider</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[338]}
