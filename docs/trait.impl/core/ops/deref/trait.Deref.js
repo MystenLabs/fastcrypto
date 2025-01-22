6fae316a7807231f00ff65f8bed9b56fdfc7b753
jonas-lj <jonas-lj@users.noreply.github.com>
2025-01-22 12:19:31 +0000

    deploy: 0acf0ff1a163c60e0dec1e16e4fbad4a4cf853bd

--- docs/trait.impl/core/ops/deref/trait.Deref.js
+++ docs/trait.impl/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["fastcrypto_vdf",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"fastcrypto_vdf/class_group/discriminant/struct.DISCRIMINANT_3072.html\" title=\"struct fastcrypto_vdf::class_group::discriminant::DISCRIMINANT_3072\">DISCRIMINANT_3072</a>"]]]]);
+    var implementors = Object.fromEntries([["fastcrypto_vdf",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"fastcrypto_vdf/class_group/discriminant/struct.DISCRIMINANT_3072.html\" title=\"struct fastcrypto_vdf::class_group::discriminant::DISCRIMINANT_3072\">DISCRIMINANT_3072</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
