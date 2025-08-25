ec0903b79609986f35fc88f110d40f90ea165170
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2025-08-25 21:15:15 +0200

    check test vector

--- fastcrypto/src/groups/secp256k1.rs
+++ fastcrypto/src/groups/secp256k1.rs
@@ -12,7 +12,7 @@ use crate::serde_helpers::ToFromByteArray;
 use crate::serialize_deserialize_with_to_from_byte_array;
 use crate::traits::AllowedRng;
 use ark_ec::{Group, ScalarMul, VariableBaseMSM};
-use ark_ff::{Field, One, UniformRand, Zero};
+use ark_ff::{BigInt, Field, One, PrimeField, UniformRand, Zero};
 use ark_secp256k1::{Affine, Fq, Fr, Projective};
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use derive_more::{Add, From, Neg, Sub};
@@ -22,6 +22,7 @@ use k256::elliptic_curve::hash2curve::{ExpandMsgXmd, GroupDigest};
 use k256::elliptic_curve::sec1::ToEncodedPoint;
 use k256::elliptic_curve::Group as GroupTrait;
 use k256::Secp256k1;
+use num_bigint::BigUint;
 use serde::{de, Deserialize};
 use std::ops::{Div, Mul};
 
@@ -150,6 +151,17 @@ impl HashToGroupElement for ProjectivePoint {
 #[derive(Default, Clone, Copy, Debug, PartialEq, Eq, From, Add, Sub, Neg, GroupOpsExtend)]
 pub struct Scalar(pub(crate) Fr);
 
+impl Scalar {
+    /// Create a scalar from a big-endian byte representation, reducing it modulo the group order if necessary.
+    pub fn from_bytes_mod_order(bytes: &[u8; SCALAR_SIZE_IN_BYTES]) -> Self {
+        Scalar(Fr::from_be_bytes_mod_order(bytes.as_slice()))
+    }
+
+    pub fn as_big_uint(&self) -> BigInt<4> {
+        self.0.into_bigint()
+    }
+}
+
 impl GroupElement for Scalar {
     type ScalarType = Scalar;
 
@@ -221,3 +233,218 @@ impl ToLittleEndianBytes for Scalar {
 }
 
 serialize_deserialize_with_to_from_byte_array!(Scalar);
+
+mod schnorr {
+    use crate::groups::secp256k1::{ProjectivePoint, Scalar};
+    use crate::groups::GroupElement;
+    use crate::hash::{HashFunction, Sha256};
+    use ark_ec::{AffineRepr, CurveGroup};
+    use ark_ff::{BigInteger, PrimeField};
+    use ark_secp256k1::{Affine, Fq, Fr, Projective};
+    use digest::Mac;
+    use k256::ecdsa::signature::Signer;
+    use k256::ecdsa::signature::Verifier;
+    use num_bigint::BigUint;
+
+    fn hash(name: &str, data: &[u8]) -> [u8; 32] {
+        use sha2::Digest;
+        let mut hasher = sha2::Sha256::new();
+        let tag_hash = sha2::Sha256::digest(name.as_bytes());
+        hasher.update(&tag_hash);
+        hasher.update(&tag_hash);
+        hasher.update(data);
+        hasher.finalize().into()
+    }
+
+    fn hash_to_scalar(name: &str, data: &[u8]) -> Scalar {
+        Scalar::from_bytes_mod_order(&hash(name, data))
+    }
+
+    fn int(be_bytes: &[u8; 32]) -> Option<Scalar> {
+        let x = BigUint::from_bytes_be(be_bytes);
+        match Fr::try_from(x) {
+            Ok(x) => Some(Scalar(x)),
+            Err(_) => None,
+        }
+    }
+
+    fn x(point: &ProjectivePoint) -> [u8; 32] {
+        let affine = point.0.into_affine();
+        affine
+            .x()
+            .unwrap()
+            .into_bigint()
+            .to_bytes_be()
+            .try_into()
+            .unwrap()
+    }
+
+    fn y(point: &ProjectivePoint) -> [u8; 32] {
+        let affine = point.0.into_affine();
+        affine
+            .y()
+            .unwrap()
+            .into_bigint()
+            .to_bytes_be()
+            .try_into()
+            .unwrap()
+    }
+
+    fn bytes_point(point: &ProjectivePoint) -> [u8; 32] {
+        x(point)
+    }
+
+    fn bytes_scalar(scalar: &Scalar) -> [u8; 32] {
+        scalar.0.into_bigint().to_bytes_be().try_into().unwrap()
+    }
+
+    fn has_even_y(point: &ProjectivePoint) -> bool {
+        let affine = point.0.into_affine();
+        affine.y().unwrap().into_bigint().is_even()
+    }
+
+    fn lift_x(x: &[u8; 32]) -> Option<ProjectivePoint> {
+        let x = BigUint::from_bytes_be(x);
+        match Fq::try_from(x) {
+            Ok(x) => match Affine::get_ys_from_x_unchecked(x.clone()) {
+                Some((y1, y2)) => {
+                    // y2 = n - y1 so one of them must be even
+                    let even_y = if y1.clone().into_bigint().is_even() {
+                        y1
+                    } else {
+                        y2
+                    };
+                    Some(ProjectivePoint(Projective::from(Affine::new(x, even_y))))
+                }
+                None => None,
+            },
+            Err(_) => None,
+        }
+    }
+
+    fn xor(a: &[u8; 32], b: &[u8; 32]) -> [u8; 32] {
+        let mut res = [0u8; 32];
+        for i in 0..32 {
+            res[i] = a[i] ^ b[i];
+        }
+        res
+    }
+
+    fn sign(sk: &Scalar, msg: &[u8], aad: &[u8]) -> Option<(ProjectivePoint, Scalar)> {
+        println!("msg: {}", hex::encode(msg));
+        println!("aad: {}", hex::encode(aad));
+        let p = ProjectivePoint::generator() * *sk;
+        let d = if has_even_y(&p) { *sk } else { -*sk };
+
+        let aux_hash = hash("BIP0340/aux", aad);
+        println!("aux hash: {}", hex::encode(aux_hash));
+        let t = xor(&bytes_scalar(&d), &aux_hash);
+        println!("t: {}", hex::encode(t));
+
+        let k_prime = hash_to_scalar("BIP0340/nonce", &[&t, &bytes_point(&p), msg].concat());
+        println!("k': {}", hex::encode(bytes_scalar(&k_prime)));
+        if k_prime.as_big_uint().is_zero() {
+            return None;
+        }
+
+        let r = ProjectivePoint::generator() * k_prime;
+        println!("R: ({}, {})", hex::encode(x(&r)), hex::encode(y(&r)));
+        let k = if has_even_y(&r) { k_prime } else { -k_prime };
+        println!("k: {}", hex::encode(bytes_scalar(&k)));
+        let e = hash_to_scalar(
+            "BIP0340/challenge",
+            &[&bytes_point(&r), &bytes_point(&p), msg].concat(),
+        );
+        println!("e: {}", hex::encode(bytes_scalar(&e)));
+
+        let s = k + e * d;
+        println!("s: {}", hex::encode(bytes_scalar(&s)));
+
+        Some((r, s))
+    }
+
+    fn signature_bytes((r, s): &(ProjectivePoint, Scalar)) -> [u8; 64] {
+        [bytes_point(r), bytes_scalar(s)]
+            .concat()
+            .try_into()
+            .unwrap()
+    }
+
+    #[test]
+    fn test_generator() {
+        let x_bytes: [u8; 32] =
+            hex::decode("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798")
+                .unwrap()
+                .try_into()
+                .unwrap();
+        let y_bytes: [u8; 32] =
+            hex::decode("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8")
+                .unwrap()
+                .try_into()
+                .unwrap();
+
+        // Test that the generator is aligned with the specs.
+        assert_eq!(x_bytes, x(&ProjectivePoint::generator()));
+        assert_eq!(y_bytes, y(&ProjectivePoint::generator()));
+        assert!(has_even_y(&ProjectivePoint::generator()));
+
+        assert_eq!(lift_x(&x_bytes), Some(ProjectivePoint::generator()));
+    }
+
+    #[test]
+    fn reference() {
+        let sk = k256::schnorr::SigningKey::from_bytes(
+            &hex::decode("B7E151628AED2A6ABF7158809CF4F3C762E7160F38B4DA56A784D9045190CFEF")
+                .unwrap(),
+        )
+        .unwrap();
+        let pk = k256::schnorr::VerifyingKey::from_bytes(
+            &hex::decode("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659")
+                .unwrap(),
+        )
+        .unwrap();
+
+        let aux_rand =
+            hex::decode("0000000000000000000000000000000000000000000000000000000000000001")
+                .unwrap()
+                .try_into()
+                .unwrap();
+        let msg = hex::decode("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89")
+            .unwrap()
+            .try_into()
+            .unwrap();
+        let signature = sk.try_sign_prehashed(&msg, &aux_rand).unwrap();
+        assert!(pk.verify_prehashed(&msg, &signature).is_ok());
+
+        let expected_signature = hex::decode("6896BD60EEAE296DB48A229FF71DFE071BDE413E6D43F917DC8DCF8C78DE33418906D11AC976ABCCB20B091292BFF4EA897EFCB639EA871CFA95F6DE339E4B0A").unwrap();
+        assert_eq!(signature.as_bytes().to_vec(), expected_signature);
+    }
+
+    #[test]
+    fn test_schnorr() {
+        // Test vector 1 from https://github.com/bitcoin/bips/blob/master/bip-0340/test-vectors.csv
+        let sk = int(&hex::decode(
+            "B7E151628AED2A6ABF7158809CF4F3C762E7160F38B4DA56A784D9045190CFEF",
+        )
+        .unwrap()
+        .try_into()
+        .unwrap())
+        .unwrap();
+        let pk = lift_x(
+            &hex::decode("DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659")
+                .unwrap()
+                .try_into()
+                .unwrap(),
+        );
+        let aux_rand =
+            hex::decode("0000000000000000000000000000000000000000000000000000000000000001")
+                .unwrap();
+        let msg = hex::decode("243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89")
+            .unwrap();
+        let expected_signature = hex::decode("6896BD60EEAE296DB48A229FF71DFE071BDE413E6D43F917DC8DCF8C78DE33418906D11AC976ABCCB20B091292BFF4EA897EFCB639EA871CFA95F6DE339E4B0A").unwrap();
+
+        let signature = sign(&sk, &msg, &aux_rand).unwrap();
+        let signature_bytes = signature_bytes(&signature);
+        assert_eq!(expected_signature, signature_bytes);
+    }
+}
