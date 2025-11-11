e229612f4b640c2adb9c6fc965bcc7a18b62c2b5
StefPler <StefPler@users.noreply.github.com>
2025-11-11 14:40:09 +0000

    deploy: b3eb689f400a4a4f9f5a69f5562bbe6bb847f71c

--- docs/trait.impl/core/error/trait.Error.js
+++ docs/trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["fastcrypto",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"fastcrypto/error/enum.FastCryptoError.html\" title=\"enum fastcrypto::error::FastCryptoError\">FastCryptoError</a>"]]]]);
+    var implementors = Object.fromEntries([["fastcrypto",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"fastcrypto/error/enum.FastCryptoError.html\" title=\"enum fastcrypto::error::FastCryptoError\">FastCryptoError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
