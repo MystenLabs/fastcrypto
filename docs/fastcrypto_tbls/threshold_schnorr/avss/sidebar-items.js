09a7a36f639cdbf3e933badc03901cdcf883279d
jonas-lj <jonas-lj@users.noreply.github.com>
2025-11-19 18:54:11 +0000

    deploy: 743b5f2bc0e09cef386286271e926f99bf3ff1fa

--- docs/fastcrypto_tbls/threshold_schnorr/avss/sidebar-items.js
+++ docs/fastcrypto_tbls/threshold_schnorr/avss/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ProcessedMessage"],"struct":["Dealer","Message","Receiver","ReceiverOutput","SharesForNode"]};
+window.SIDEBAR_ITEMS = {"enum":["ProcessedMessage"],"struct":["Dealer","Message","PartialOutput","Receiver","ReceiverOutput","SharesForNode"]};
