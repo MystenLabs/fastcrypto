f4c69749d4b8abc2beafd1f6d1c2b8f576eb2e4f
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2026-01-16 12:14:10 +0100

    tests

--- fastcrypto/src/tests/bulletproofs_tests.rs
+++ fastcrypto/src/tests/bulletproofs_tests.rs
@@ -10,15 +10,9 @@ const TEST_DOMAIN: &[u8; 7] = b"NARWHAL";
 #[test]
 fn test_range_proof_valid() {
     let upper_bound: usize = 64;
-    let blinding = RistrettoScalar::from_byte_array(&[
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        0, 1,
-    ])
-    .unwrap();
-
+    let blinding = RistrettoScalar::from(7);
     let range_proof =
         RangeProof::prove_bit_length(1u64, blinding, upper_bound, TEST_DOMAIN).unwrap();
-
     assert!(range_proof
         .verify_bit_length(upper_bound, TEST_DOMAIN)
         .is_ok());
@@ -27,12 +21,7 @@ fn test_range_proof_valid() {
 #[test]
 fn test_handle_prove_invalid_upper_bound() {
     let invalid_upper_bound = 22;
-    let blinding = RistrettoScalar::from_byte_array(&[
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        0, 1,
-    ])
-    .unwrap();
-
+    let blinding = RistrettoScalar::from(7);
     assert!(
         RangeProof::prove_bit_length(1u64, blinding, invalid_upper_bound, TEST_DOMAIN).is_err()
     );
@@ -42,22 +31,16 @@ fn test_handle_prove_invalid_upper_bound() {
 fn test_handle_verify_invalid_upper_bound() {
     let valid_upper_bound = 64;
     let invalid_upper_bound = 22;
-    let blinding = RistrettoScalar::from_byte_array(&[
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        0, 1,
-    ])
-    .unwrap();
-
+    let blinding = RistrettoScalar::from(7);
     let range_proof =
         RangeProof::prove_bit_length(1u64, blinding, valid_upper_bound, TEST_DOMAIN).unwrap();
-
     assert!(range_proof
         .verify_bit_length(invalid_upper_bound, TEST_DOMAIN)
         .is_err());
 }
 
 #[test]
-fn test_aggregated_range_proof_valid() {
+fn test_aggregated_range_proof() {
     let upper_bound: usize = 64;
     let blindings = [
         RistrettoScalar::from(7),
@@ -78,7 +61,7 @@ fn test_aggregated_range_proof_valid() {
 }
 
 #[test]
-fn test_aggregated_range_proof_invalid() {
+fn test_aggregated_range_proof_test_upper_bounds() {
     let upper_bound: usize = 8;
     let blindings = [
         RistrettoScalar::from(7),
@@ -93,6 +76,15 @@ fn test_aggregated_range_proof_invalid() {
         TEST_DOMAIN,
     )
     .is_err());
+
+    let upper_bound: usize = 16;
+    assert!(AggregateRangeProof::prove_bit_length(
+        &[1u64, 2u64, 3u64, 256u64],
+        &blindings,
+        upper_bound,
+        TEST_DOMAIN,
+    )
+    .is_ok());
 }
 
 use crate::groups::ristretto255::RistrettoScalar;
