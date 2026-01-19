0c922f9f9f4254bcf4de657e3b3ac78c9d6d9541
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2026-01-19 15:26:10 +0100

    license

--- fastcrypto/src/pedersen.rs
+++ fastcrypto/src/pedersen.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::error::FastCryptoError::InvalidProof;
 use crate::error::FastCryptoResult;
 use crate::groups::ristretto255::{RistrettoPoint, RistrettoScalar};
