1f683c8d37a18ea132db4de553ccfc08adf67be0
Philipp Jovanovic <philipp@jovanovic.io>
2026-03-02 17:08:15 +0100

    fix: license checker still complaining...

--- fastcrypto-zkp/src/bp3/mod.rs
+++ fastcrypto-zkp/src/bp3/mod.rs
@@ -1,4 +1,5 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
+
 mod util;
 pub mod wnla;
--- fastcrypto-zkp/src/bp3/wnla.rs
+++ fastcrypto-zkp/src/bp3/wnla.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 #![allow(non_snake_case)]
 
 use crate::bp3::util::*;
