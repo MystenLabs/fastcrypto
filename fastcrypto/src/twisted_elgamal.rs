965d1b03e5db42309f8f1e7d282ed48049e5a6e9
Philipp Jovanovic <philipp@jovanovic.io>
2026-03-23 11:46:37 +0100

    Update fastcrypto/src/twisted_elgamal.rs
    Co-authored-by: Jonas Lindstrøm <jonas-lj@users.noreply.github.com>

--- fastcrypto/src/twisted_elgamal.rs
+++ fastcrypto/src/twisted_elgamal.rs
@@ -334,7 +334,7 @@ pub struct VerifiableKeyEncapsulation {
 impl VerifiableKeyEncapsulation {
     /// Verifiably encrypt a private key to multiple recipient public keys
     pub fn batch_seal(
-        public_keys: &[&PublicKey],
+        public_keys: &[PublicKey],
         private_key: &PrivateKey,
         rng: &mut impl AllowedRng,
     ) -> VerifiableKeyEncapsulation {
