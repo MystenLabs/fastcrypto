dfa186db859956369cfe99ad6b39d33df9e29d6d
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2026-04-10 14:36:13 +0200

    Reinstate test_get_jwks as part of zk-login-e2e flow

--- fastcrypto-zkp/src/bn254/unit_tests/zk_login_e2e_tests.rs
+++ fastcrypto-zkp/src/bn254/unit_tests/zk_login_e2e_tests.rs
@@ -368,3 +368,37 @@ async fn test_end_to_end_test_issuer(test_input: TestInputStruct) {
     }
     .await;
 }
+
+#[tokio::test]
+async fn test_get_jwks() {
+    let client = reqwest::Client::new();
+    for p in [
+        OIDCProvider::Facebook,
+        OIDCProvider::Google,
+        OIDCProvider::Twitch,
+        OIDCProvider::Slack,
+        OIDCProvider::Kakao,
+        OIDCProvider::Apple,
+        OIDCProvider::Microsoft,
+        OIDCProvider::KarrierOne,
+        OIDCProvider::Credenza3,
+        OIDCProvider::Playtron,
+        OIDCProvider::Threedos,
+        OIDCProvider::Onefc,
+        OIDCProvider::FanTV,
+        // OIDCProvider::Arden, // TODO: disabling until the service is up again
+        OIDCProvider::EveFrontier,
+        OIDCProvider::TestEveFrontier,
+        OIDCProvider::AwsTenant(("eu-west-3".to_string(), "eu-west-3_gGVCx53Es".to_string())), //Trace
+        OIDCProvider::AwsTenant((
+            "ap-southeast-1".to_string(),
+            "ap-southeast-1_2QQPyQXDz".to_string(),
+        )), // Decot
+    ] {
+        let res = fetch_jwks(&p, &client, true).await;
+        assert!(res.is_ok());
+        res.unwrap().iter().for_each(|e| {
+            assert_eq!(e.0.iss, p.get_config().iss);
+        });
+    }
+}
