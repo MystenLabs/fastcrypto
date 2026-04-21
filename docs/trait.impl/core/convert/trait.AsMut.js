d32048a9c1876076c1415dbfe429254f79fffd01
jonas-lj <jonas-lj@users.noreply.github.com>
2026-04-21 11:56:56 +0000

    deploy: 54188fee0369ce472cd426727aa403b080cc8296

--- docs/trait.impl/core/convert/trait.AsMut.js
+++ docs/trait.impl/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["fastcrypto_zkp",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.94.1/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"fastcrypto_zkp/struct.FrRepr.html\" title=\"struct fastcrypto_zkp::FrRepr\">FrRepr</a>",0]]]]);
+    const implementors = Object.fromEntries([["fastcrypto_zkp",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.95.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"fastcrypto_zkp/struct.FrRepr.html\" title=\"struct fastcrypto_zkp::FrRepr\">FrRepr</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
