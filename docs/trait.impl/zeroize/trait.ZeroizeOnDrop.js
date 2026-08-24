8728aaed3ca101014b8ec8251e1726100fbed4b6
mahdi-mysten <mahdi-mysten@users.noreply.github.com>
2026-08-24 12:27:34 +0000

    deploy: 38a4f3c61aad1a0060ad45789082532ca54284f2

--- docs/trait.impl/zeroize/trait.ZeroizeOnDrop.js
+++ docs/trait.impl/zeroize/trait.ZeroizeOnDrop.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["fastcrypto",[["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto/bls12381/min_pk/struct.BLS12381PrivateKey.html\" title=\"struct fastcrypto::bls12381::min_pk::BLS12381PrivateKey\">BLS12381PrivateKey</a>",0],["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto/bls12381/min_sig/struct.BLS12381PrivateKey.html\" title=\"struct fastcrypto::bls12381::min_sig::BLS12381PrivateKey\">BLS12381PrivateKey</a>",0],["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto/secp256k1/struct.Secp256k1PrivateKey.html\" title=\"struct fastcrypto::secp256k1::Secp256k1PrivateKey\">Secp256k1PrivateKey</a>",0],["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto/secp256r1/struct.Secp256r1PrivateKey.html\" title=\"struct fastcrypto::secp256r1::Secp256r1PrivateKey\">Secp256r1PrivateKey</a>",0]]]]);
+    const implementors = Object.fromEntries([["fastcrypto",[["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto/bls12381/min_sig/struct.BLS12381PrivateKey.html\" title=\"struct fastcrypto::bls12381::min_sig::BLS12381PrivateKey\">BLS12381PrivateKey</a>",0],["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto/bls12381/min_pk/struct.BLS12381PrivateKey.html\" title=\"struct fastcrypto::bls12381::min_pk::BLS12381PrivateKey\">BLS12381PrivateKey</a>",0],["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto/secp256k1/struct.Secp256k1PrivateKey.html\" title=\"struct fastcrypto::secp256k1::Secp256k1PrivateKey\">Secp256k1PrivateKey</a>",0],["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto/secp256r1/struct.Secp256r1PrivateKey.html\" title=\"struct fastcrypto::secp256r1::Secp256r1PrivateKey\">Secp256r1PrivateKey</a>",0]]],["fastcrypto_pq",[["impl ZeroizeOnDrop for <a class=\"struct\" href=\"fastcrypto_pq/mldsa65/struct.MLDSA65PrivateKey.html\" title=\"struct fastcrypto_pq::mldsa65::MLDSA65PrivateKey\">MLDSA65PrivateKey</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[818]}
+//{"start":59,"fragment_lengths":[818,211]}
